0bdfd671f427c6040edb209a6d16c78c78e0234d
Manuel Luis Sanmartín Rozada <manuel.luis@gmail.com>
2024-03-19 23:01:57 +0100

    Plugin contextswitch: migrate plugin

--- src/plugins/contextswitch/contextswitch.c
+++ src/plugins/contextswitch/contextswitch.c
@@ -1,41 +1,24 @@
-/**
- * collectd - src/contextswitch.c
- * Copyright (C) 2009  Patrik Weiskircher
- * Copyright (C) 2010  Kimo Rosenbaum
- *
- * This program is free software; you can redistribute it and/or modify it
- * under the terms of the GNU General Public License as published by the
- * Free Software Foundation; only version 2 of the License is applicable.
- *
- * This program is distributed in the hope that it will be useful, but
- * WITHOUT ANY WARRANTY; without even the implied warranty of
- * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU
- * General Public License for more details.
- *
- * You should have received a copy of the GNU General Public License along
- * with this program; if not, write to the Free Software Foundation, Inc.,
- * 51 Franklin St, Fifth Floor, Boston, MA  02110-1301 USA
- *
- * Authors:
- *   Patrik Weiskircher <weiskircher at inqnet.at>
- *   Kimo Rosenbaum <http://github.com/kimor79>
- **/
-
-#include "collectd.h"
+// SPDX-License-Identifier: GPL-2.0-only
+// SPDX-FileCopyrightText: Copyright (C) 2009 Patrik Weiskircher
+// SPDX-FileCopyrightText: Copyright (C) 2010 Kimo Rosenbaum
+// SPDX-FileCopyrightText: Copyright (C) 2022-2024 Manuel Sanmartín
+// SPDX-FileContributor: Patrik Weiskircher <weiskircher at inqnet.at>
+// SPDX-FileContributor: Kimo Rosenbaum <http://github.com/kimor79>
+// SPDX-FileContributor: Manuel Sanmartín <manuel.luis at gmail.com>
 
 #include "plugin.h"
-#include "utils/common/common.h"
+#include "libutils/common.h"
 
 #if defined(HAVE_SYSCTLBYNAME) && defined(HAVE_SYS_SYSCTL_H)
 #include <sys/sysctl.h>
 /* no global variables */
 /* #endif HAVE_SYSCTLBYNAME */
 
-#elif KERNEL_LINUX
-/* no global variables */
+#elif defined(KERNEL_LINUX)
+static char *path_proc_stat;
 /* #endif KERNEL_LINUX */
 
-#elif HAVE_PERFSTAT
+#elif defined(HAVE_PERFSTAT)
 #include <libperfstat.h>
 #include <sys/protosw.h>
 /* #endif HAVE_PERFSTAT */
@@ -44,103 +27,107 @@
 #error "No applicable input method."
 #endif
 
-static void cs_submit(derive_t context_switches) {
-  metric_family_t fam = {
-      .name = "context_switches_total",
-      .type = METRIC_TYPE_COUNTER,
-  };
+static  metric_family_t fam = {
+    .name = "system_context_switches",
+    .type = METRIC_TYPE_COUNTER,
+    .help = "Total number of context switches across all CPUs."
+};
 
-  metric_family_metric_append(&fam, (metric_t){
-                                        .value.counter = context_switches,
-                                    });
+static int cs_read(void)
+{
+    uint64_t context_switches = 0;
 
-  int status = plugin_dispatch_metric_family(&fam);
-  if (status != 0) {
-    ERROR("contextswitch plugin: plugin_dispatch_metric_family failed: %s",
-          STRERROR(status));
-  }
-
-  metric_family_metric_reset(&fam);
-}
-
-static int cs_read(void) {
-#if HAVE_SYSCTLBYNAME
-  int value = 0;
-  size_t value_len = sizeof(value);
-  int status;
-
-  status = sysctlbyname("vm.stats.sys.v_swtch", &value, &value_len,
-                        /* new pointer = */ NULL, /* new length = */ 0);
-  if (status != 0) {
-    ERROR("contextswitch plugin: sysctlbyname "
-          "(vm.stats.sys.v_swtch) failed");
-    return -1;
-  }
-
-  cs_submit(value);
-  /* #endif HAVE_SYSCTLBYNAME */
-
-#elif KERNEL_LINUX
-  FILE *fh;
-  char buffer[64];
-  int numfields;
-  char *fields[3];
-  derive_t result = 0;
-  int status = -2;
-
-  fh = fopen("/proc/stat", "r");
-  if (fh == NULL) {
-    ERROR("contextswitch plugin: unable to open /proc/stat: %s", STRERRNO);
-    return -1;
-  }
-
-  while (fgets(buffer, sizeof(buffer), fh) != NULL) {
-    char *endptr;
-
-    numfields = strsplit(buffer, fields, STATIC_ARRAY_SIZE(fields));
-    if (numfields != 2)
-      continue;
-
-    if (strcmp("ctxt", fields[0]) != 0)
-      continue;
-
-    errno = 0;
-    endptr = NULL;
-    result = (derive_t)strtoll(fields[1], &endptr, /* base = */ 10);
-    if ((endptr == fields[1]) || (errno != 0)) {
-      ERROR("contextswitch plugin: Cannot parse ctxt value: %s", fields[1]);
-      status = -1;
-      break;
+#ifdef HAVE_SYSCTLBYNAME
+    int value = 0;
+    size_t value_len = sizeof(value);
+    int status = sysctlbyname("vm.stats.sys.v_swtch", &value, &value_len,
+                          /* new pointer = */ NULL, /* new length = */ 0);
+    if (status != 0) {
+        PLUGIN_ERROR("sysctlbyname (vm.stats.sys.v_swtch) failed");
+        return -1;
     }
 
-    cs_submit(result);
+    context_switches = (uint64_t)value;
+
+#elif defined(KERNEL_LINUX)
+    FILE *fh = fopen(path_proc_stat, "r");
+    if (fh == NULL) {
+        PLUGIN_ERROR("unable to open '%s': %s", path_proc_stat, STRERRNO);
+        return -1;
+    }
+
+    char buffer[64];
+    char *fields[3];
+    int status = -2;
+    while (fgets(buffer, sizeof(buffer), fh) != NULL) {
+        int numfields = strsplit(buffer, fields, STATIC_ARRAY_SIZE(fields));
+        if (numfields != 2)
+            continue;
+
+        if (strcmp("ctxt", fields[0]) != 0)
+            continue;
+
+        errno = 0;
+        char *endptr = NULL;
+        context_switches = (uint64_t)strtoll(fields[1], &endptr, /* base = */ 10);
+        if ((endptr == fields[1]) || (errno != 0)) {
+            PLUGIN_ERROR("Cannot parse ctxt value: %s", fields[1]);
+            status = -1;
+            break;
+        }
+
+        status = 0;
+        break;
+    }
+    fclose(fh);
+
+    if (status == -2) {
+        PLUGIN_ERROR("Unable to find context switch value.");
+        return status;
+    }
+
+#elif defined(HAVE_PERFSTAT)
+    perfstat_cpu_total_t perfcputotal;
+    int status = perfstat_cpu_total(NULL, &perfcputotal, sizeof(perfstat_cpu_total_t), 1);
+    if (status < 0) {
+        PLUGIN_ERROR("perfstat_cpu_total: %s", STRERRNO);
+        return -1;
+    }
+
+    context_switches = (uint64_t)perfcputotal.pswitch;
     status = 0;
-    break;
-  }
-  fclose(fh);
+#endif
 
-  if (status == -2)
-    ERROR("contextswitch plugin: Unable to find context switch value.");
-    /* #endif  KERNEL_LINUX */
+    metric_family_append(&fam, VALUE_COUNTER(context_switches), NULL, NULL);
+    plugin_dispatch_metric_family(&fam, 0);
 
-#elif HAVE_PERFSTAT
-  int status = 0;
-  perfstat_cpu_total_t perfcputotal;
-
-  status =
-      perfstat_cpu_total(NULL, &perfcputotal, sizeof(perfstat_cpu_total_t), 1);
-  if (status < 0) {
-    ERROR("contextswitch plugin: perfstat_cpu_total: %s", STRERRNO);
-    return -1;
-  }
-
-  cs_submit(perfcputotal.pswitch);
-  status = 0;
-#endif /* defined(HAVE_PERFSTAT) */
-
-  return status;
+    return status;
 }
 
-void module_register(void) {
-  plugin_register_read("contextswitch", cs_read);
-} /* void module_register */
+#ifdef KERNEL_LINUX
+static int cs_init(void)
+{
+    path_proc_stat = plugin_procpath("stat");
+    if (path_proc_stat == NULL) {
+        PLUGIN_ERROR("Cannot get proc path.");
+        return -1;
+    }
+
+    return 0;
+}
+
+static int cs_shutdown(void)
+{
+    free(path_proc_stat);
+    return 0;
+}
+#endif
+
+void module_register(void)
+{
+#ifdef KERNEL_LINUX
+    plugin_register_init("contextswitch", cs_init);
+    plugin_register_shutdown("contextswitch", cs_shutdown);
+#endif
+    plugin_register_read("contextswitch", cs_read);
+}
