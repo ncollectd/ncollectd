357b9c1804a4faa2d823acdca0bfe15b6cb7f565
Manuel Luis Sanmartín Rozada <manuel.luis@gmail.com>
2024-03-19 23:02:02 +0100

    Plugin syncproxy: upgrade plugin.

--- src/plugins/synproxy/synproxy.c
+++ src/plugins/synproxy/synproxy.c
@@ -1,112 +1,139 @@
-/**
- * collectd - src/synproxy.c
- * Copyright (C) 2017 Marek Becka
- *
- * This program is free software; you can redistribute it and/or modify it
- * under the terms of the GNU General Public License as published by the
- * Free Software Foundation; only version 2 of the License is applicable.
- *
- * This program is distributed in the hope that it will be useful, but
- * WITHOUT ANY WARRANTY; without even the implied warranty of
- * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU
- * General Public License for more details.
- *
- * You should have received a copy of the GNU General Public License along
- * with this program; if not, write to the Free Software Foundation, Inc.,
- * 51 Franklin St, Fifth Floor, Boston, MA  02110-1301 USA
- *
- * Authors:
- *   Marek Becka <https://github.com/marekbecka>
- **/
-
-#include "collectd.h"
+// SPDX-License-Identifier: GPL-2.0-only
+// SPDX-FileCopyrightText: Copyright (C) 2017 Marek Becka
+// SPDX-FileCopyrightText: Copyright (C) 2022-2024 Manuel Sanmartín
+// SPDX-FileContributor: Marek Becka <https://github.com/marekbecka>
+// SPDX-FileContributor: Manuel Sanmartín <manuel.luis at gmail.com>
 
 #include "plugin.h"
-#include "utils/common/common.h"
+#include "libutils/common.h"
 
-#if !KERNEL_LINUX
+#ifndef KERNEL_LINUX
 #error "No applicable input method."
 #endif
 
 #define SYNPROXY_FIELDS 6
 
-static const char *synproxy_stat_path = "/proc/net/stat/synproxy";
+static char *path_proc_synproxy;
 
-static const char *column_names[SYNPROXY_FIELDS] = {
-    "entries", "syn_received",   "invalid",
-    "valid",   "retransmission", "reopened"};
-static const char *column_types[SYNPROXY_FIELDS] = {
-    "current_connections", "connections", "cookies", "cookies", "cookies",
-    "connections"};
+enum {
+    FAM_SYNPROXY_CONNECTIONS_SYN_RECEIVED,
+    FAM_SYNPROXY_COOKIES_INVALID,
+    FAM_SYNPROXY_COOKIES_VALID,
+    FAM_SYNPROXY_COOKIES_RETRANSMISSION,
+    FAM_SYNPROXY_CONNECTIONS_REOPENED,
+    FAM_SYNPROXY_MAX,
+};
 
-static void synproxy_submit(value_t *results) {
-  value_list_t vl = VALUE_LIST_INIT;
+static metric_family_t fams[FAM_SYNPROXY_MAX] = {
+    [FAM_SYNPROXY_CONNECTIONS_SYN_RECEIVED] = {
+        .name = "system_synproxy_connections_syn_received",
+        .type = METRIC_TYPE_COUNTER,
+        .help = "Number of SYN received.",
+    },
+    [FAM_SYNPROXY_COOKIES_INVALID] = {
+        .name = "system_synproxy_cookies_invalid",
+        .type = METRIC_TYPE_COUNTER,
+        .help = "Number of invalid cookies.",
+    },
+    [FAM_SYNPROXY_COOKIES_VALID] = {
+        .name = "system_synproxy_cookies_valid",
+        .type = METRIC_TYPE_COUNTER,
+        .help = "Number of valid cookies.",
+    },
+    [FAM_SYNPROXY_COOKIES_RETRANSMISSION] = {
+        .name = "system_synproxy_cookies_retransmission",
+        .type = METRIC_TYPE_COUNTER,
+        .help = "Nymber of cookies retransmitted.",
+    },
+    [FAM_SYNPROXY_CONNECTIONS_REOPENED] = {
+        .name = "system_synproxy_connections_reopened",
+        .type = METRIC_TYPE_COUNTER,
+        .help = "Number of connections reopened.",
+    },
+};
 
-  /* 1st column (entries) is hardcoded to 0 in kernel code */
-  for (size_t n = 1; n < SYNPROXY_FIELDS; n++) {
-    vl.values = &results[n];
-    vl.values_len = 1;
-
-    sstrncpy(vl.plugin, "synproxy", sizeof(vl.plugin));
-    sstrncpy(vl.type, column_types[n], sizeof(vl.type));
-    sstrncpy(vl.type_instance, column_names[n], sizeof(vl.type_instance));
-
-    plugin_dispatch_values(&vl);
-  }
-}
-
-static int synproxy_read(void) {
-  char buf[1024];
-  value_t results[SYNPROXY_FIELDS];
-  int is_header = 1, status = 0;
-
-  FILE *fh = fopen(synproxy_stat_path, "r");
-  if (fh == NULL) {
-    ERROR("synproxy plugin: unable to open %s", synproxy_stat_path);
-    return -1;
-  }
-
-  memset(results, 0, sizeof(results));
-
-  while (fgets(buf, sizeof(buf), fh) != NULL) {
-    char *fields[SYNPROXY_FIELDS], *endprt;
-
-    if (is_header) {
-      is_header = 0;
-      continue;
-    }
-
-    int numfields = strsplit(buf, fields, STATIC_ARRAY_SIZE(fields));
-    if (numfields != SYNPROXY_FIELDS) {
-      ERROR("synproxy plugin: unexpected number of columns in %s",
-            synproxy_stat_path);
-      status = -1;
-      break;
-    }
-
-    /* 1st column (entries) is hardcoded to 0 in kernel code */
-    for (size_t n = 1; n < SYNPROXY_FIELDS; n++) {
-      char *endptr = NULL;
-      errno = 0;
-
-      results[n].derive += strtoull(fields[n], &endprt, 16);
-      if ((endptr == fields[n]) || errno != 0) {
-        ERROR("synproxy plugin: unable to parse value: %s", fields[n]);
-        fclose(fh);
+static int synproxy_read(void)
+{
+    FILE *fh = fopen(path_proc_synproxy, "r");
+    if (fh == NULL) {
+        PLUGIN_ERROR("Unable to open '%s': %s", path_proc_synproxy, STRERRNO);
         return -1;
-      }
     }
-  }
 
-  fclose(fh);
+    value_t results[SYNPROXY_FIELDS];
+    memset(results, 0, sizeof(results));
 
-  if (status == 0) {
-    synproxy_submit(results);
-  }
+    int is_header = 1, status = 0;
+    char buf[1024];
+    while (fgets(buf, sizeof(buf), fh) != NULL) {
+        char *fields[SYNPROXY_FIELDS], *endprt;
 
-  return status;
+        if (is_header) {
+            is_header = 0;
+            continue;
+        }
+
+        int numfields = strsplit(buf, fields, STATIC_ARRAY_SIZE(fields));
+        if (numfields != SYNPROXY_FIELDS) {
+            PLUGIN_ERROR("Unexpected number of columns in %s", path_proc_synproxy);
+            status = -1;
+            break;
+        }
+
+        /* 1st column (entries) is hardcoded to 0 in kernel code */
+        for (size_t n = 1; n < SYNPROXY_FIELDS; n++) {
+            char *endptr = NULL;
+            errno = 0;
+
+            results[n] = VALUE_COUNTER(strtoull(fields[n], &endprt, 16));
+            if ((endptr == fields[n]) || errno != 0) {
+                PLUGIN_ERROR("Unable to parse value: %s", fields[n]);
+                fclose(fh);
+                return -1;
+            }
+        }
+    }
+
+    if (status == 0) {
+        metric_family_metric_append(&fams[FAM_SYNPROXY_CONNECTIONS_SYN_RECEIVED],
+                                    (metric_t){ .value = results[1]});
+        metric_family_metric_append(&fams[FAM_SYNPROXY_COOKIES_INVALID],
+                                    (metric_t){ .value = results[2]});
+        metric_family_metric_append(&fams[FAM_SYNPROXY_COOKIES_VALID],
+                                    (metric_t){ .value = results[3]});
+        metric_family_metric_append(&fams[FAM_SYNPROXY_COOKIES_RETRANSMISSION],
+                                    (metric_t){ .value = results[4]});
+        metric_family_metric_append(&fams[FAM_SYNPROXY_CONNECTIONS_REOPENED],
+                                    (metric_t){ .value = results[5]});
+
+        plugin_dispatch_metric_family_array(fams, FAM_SYNPROXY_MAX, 0);
+    }
+
+    fclose(fh);
+
+    return status;
 }
 
-void module_register(void) {
-  plugin_register_read("synproxy", synproxy_read);
-} /* void module_register */
+static int synproxy_init(void)
+{
+    path_proc_synproxy = plugin_procpath("net/stat/synproxy");
+    if (path_proc_synproxy == NULL) {
+        PLUGIN_ERROR("Cannot get proc path.");
+        return -1;
+    }
+
+    return 0;
+}
+
+static int synproxy_shutdown(void)
+{
+    free(path_proc_synproxy);
+    return 0;
+}
+
+void module_register(void)
+{
+    plugin_register_init("synproxy", synproxy_init);
+    plugin_register_read("synproxy", synproxy_read);
+    plugin_register_shutdown("synproxy", synproxy_shutdown);
+}
