96d656d947bca0390f95b892cbcb006db355207f
Manuel Luis Sanmartín Rozada <manuel.luis@gmail.com>
2024-03-19 23:01:57 +0100

    Plugin buddyinfo: migrate plugin.

--- src/plugins/buddyinfo/buddyinfo.c
+++ src/plugins/buddyinfo/buddyinfo.c
@@ -1,141 +1,130 @@
-/**
- * collectd - src/buddyinfo.c
- * Copyright (C) 2019       Asaf Kahlon
- *
- * This program is free software; you can redistribute it and/or modify it
- * under the terms of the GNU General Public License as published by the
- * Free Software Foundation; only version 2 of the License is applicable.
- *
- * This program is distributed in the hope that it will be useful, but
- * WITHOUT ANY WARRANTY; without even the implied warranty of
- * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU
- * General Public License for more details.
- *
- * You should have received a copy of the GNU General Public License along
- * with this program; if not, write to the Free Software Foundation, Inc.,
- * 51 Franklin St, Fifth Floor, Boston, MA  02110-1301 USA
- *
- * Authors:
- *   Asaf Kahlon <asafka7 at gmail.com>
- **/
-
-#include "collectd.h"
+// SPDX-License-Identifier: GPL-2.0-only
+// SPDX-FileCopyrightText Copyright (C) 2019 Asaf Kahlon
+// SPDX-FileCopyrightText: Copyright (C) 2022-2024 Manuel Sanmartín
+// SPDX-FileContributor: Asaf Kahlon <asafka7 at gmail.com>
+// SPDX-FileContributor: Manuel Sanmartín <manuel.luis at gmail.com>
 
 #include "plugin.h"
-#include "utils/common/common.h"
-#include "utils/ignorelist/ignorelist.h"
+#include "libutils/common.h"
+#include "libutils/exclist.h"
 
-#if !KERNEL_LINUX
+#ifndef KERNEL_LINUX
 #error "No applicable input method."
 #endif
 
 #include <unistd.h>
 
 #define MAX_ORDER 11
-#define BUDDYINFO_FIELDS                                                       \
-  MAX_ORDER + 4 // "Node" + node_num + "zone" + Name + (MAX_ORDER entries)
+#define BUDDYINFO_FIELDS MAX_ORDER + 4 // "Node" + node_num + "zone" + Name + (MAX_ORDER entries)
 #define NUM_OF_KB(pagesize, order) ((pagesize) / 1024) * (1 << (order))
 
-static const char *config_keys[] = {"Zone"};
-static int config_keys_num = STATIC_ARRAY_SIZE(config_keys);
+static char *path_proc_buddyinfo;
+static int pagesize;
+static exclist_t excl_zone;
 
-static ignorelist_t *ignorelist;
+static metric_family_t fam = {
+    .name = "system_buddyinfo_freepages",
+    .type = METRIC_TYPE_GAUGE,
+    .help = "Number of pages of a certain order (a certain size) "
+            "that are available at any given time."
+};
 
-static int buddyinfo_config(const char *key, const char *value) {
-  if (ignorelist == NULL) {
-    ignorelist = ignorelist_create(1);
-    if (ignorelist == NULL) {
-      ERROR("buddyinfo plugin: ignorelist_create failed");
-      return -ENOMEM;
+static int buddyinfo_read(void)
+{
+    FILE *fh = fopen(path_proc_buddyinfo, "r");
+    if (fh == NULL) {
+        PLUGIN_ERROR("Cannot open '%s': %s", path_proc_buddyinfo, STRERRNO);
+        return -1;
     }
-  }
 
-  if (strcasecmp(key, "Zone") == 0) {
-    if (ignorelist_add(ignorelist, value)) {
-      ERROR("buddyinfo plugin: cannot add value to ignorelist");
-      return -1;
+    cdtime_t submit = cdtime();
+
+    char buffer[1024];
+    while (fgets(buffer, sizeof(buffer), fh) != NULL) {
+        char *dummy = strstr(buffer, "Node");
+        if (dummy == NULL)
+            continue;
+
+        char *fields[BUDDYINFO_FIELDS];
+        int numfields = strsplit(dummy, fields, BUDDYINFO_FIELDS);
+        if (numfields != BUDDYINFO_FIELDS) {
+            PLUGIN_WARNING("Line '%s' doesn't contain %d orders, skipping...", buffer, MAX_ORDER);
+            continue;
+        }
+
+        int node_num = atoi(fields[1]);
+        char *zone = fields[3];
+
+        if (!exclist_match(&excl_zone, zone))
+            continue;
+
+        char node_name[16];
+        ssnprintf(node_name, sizeof(node_name), "%d", node_num);
+
+        for (int i = 1; i <= MAX_ORDER; i++) {
+            char pagesize_kb[8];
+            ssnprintf(pagesize_kb, sizeof(pagesize_kb), "%d", NUM_OF_KB(pagesize, i - 1));
+            metric_family_append(&fam, VALUE_GAUGE(atoi(fields[i + 3])), NULL,
+                                 &(label_pair_const_t){.name="node", .value=node_name},
+                                 &(label_pair_const_t){.name="zone", .value=zone},
+                                 &(label_pair_const_t){.name="pagesize_kb", .value=pagesize_kb},
+                                 NULL);
+        }
     }
-  } else {
-    ERROR("buddyinfo plugin: invalid option: %s", key);
-    return -1;
-  }
 
-  return 0;
+    fclose(fh);
+
+    plugin_dispatch_metric_family(&fam, submit);
+
+    return 0;
 }
 
-static int buddyinfo_read(void) {
-  FILE *fh;
-  char buffer[1024], pagesize_kb[8], node_name[16];
-  char *dummy, *zone;
-  char *fields[BUDDYINFO_FIELDS];
-  int node_num, numfields, pagesize = getpagesize();
-  metric_family_t fam = {
-      .name = "buddyinfo_freepages",
-      .type = METRIC_TYPE_GAUGE,
-  };
+static int buddyinfo_config(config_item_t *ci)
+{
+    int status = 0;
 
-  if ((fh = fopen("/proc/buddyinfo", "r")) == NULL) {
-    WARNING("buddyinfo plugin: fopen: %s", STRERRNO);
-    return -1;
-  }
+    for (int i = 0; i < ci->children_num; i++) {
+        config_item_t *child = ci->children + i;
+        if (strcasecmp(child->key, "zone") == 0) {
+            status = cf_util_exclist(child, &excl_zone);
+        } else {
+            PLUGIN_ERROR("Option '%s' in %s:%d is not allowed.",
+                          child->key, cf_get_file(child), cf_get_lineno(child));
+            status = -1;
+        }
 
-  metric_t m = {0};
-
-  while (fgets(buffer, sizeof(buffer), fh) != NULL) {
-    if (!(dummy = strstr(buffer, "Node")))
-      continue;
-
-    numfields = strsplit(dummy, fields, BUDDYINFO_FIELDS);
-    if (numfields != BUDDYINFO_FIELDS) {
-      WARNING("line %s doesn't contain %d orders, skipping...", buffer,
-              MAX_ORDER);
-      continue;
+        if (status != 0)
+            return -1;
     }
 
-    node_num = atoi(fields[1]);
-    zone = fields[3];
+    return 0;
+}
 
-    if (ignorelist_match(ignorelist, zone) != 0)
-      continue;
 
-    ssnprintf(node_name, sizeof(node_name), "%d", node_num);
-    metric_label_set(&m, "node", node_name);
-    metric_label_set(&m, "zone", zone);
+static int buddyinfo_init(void)
+{
+    pagesize = getpagesize();
 
-    for (int i = 1; i <= MAX_ORDER; i++) {
-      ssnprintf(pagesize_kb, sizeof(pagesize_kb), "%d",
-                NUM_OF_KB(pagesize, i - 1));
-      metric_label_set(&m, "pagesize_kb", pagesize_kb);
-
-      m.value.gauge = atoi(fields[i + 3]);
-      metric_family_metric_append(&fam, m);
+    path_proc_buddyinfo = plugin_procpath("buddyinfo");
+    if (path_proc_buddyinfo == NULL) {
+        PLUGIN_ERROR("Cannot get proc path.");
+        return -1;
     }
-  }
 
-  metric_reset(&m);
-
-  int status = plugin_dispatch_metric_family(&fam);
-  if (status != 0) {
-    ERROR("buddyinfo plugin: plugin_dispatch_metric_family failed: %s",
-          STRERROR(status));
-  }
-
-  metric_family_metric_reset(&fam);
-
-  fclose(fh);
-  return 0;
+    return 0;
 }
 
-static int buddyinfo_shutdown(void) {
-  ignorelist_free(ignorelist);
-
-  return 0;
+static int buddyinfo_shutdown(void)
+{
+    free(path_proc_buddyinfo);
+    exclist_reset(&excl_zone);
+    return 0;
 }
 
-void module_register(void) {
-
-  plugin_register_config("buddyinfo", buddyinfo_config, config_keys,
-                         config_keys_num);
-  plugin_register_read("buddyinfo", buddyinfo_read);
-  plugin_register_shutdown("buddy", buddyinfo_shutdown);
+void module_register(void)
+{
+    plugin_register_init("buddyinfo", buddyinfo_init);
+    plugin_register_config("buddyinfo", buddyinfo_config);
+    plugin_register_read("buddyinfo", buddyinfo_read);
+    plugin_register_shutdown("buddy", buddyinfo_shutdown);
 }
