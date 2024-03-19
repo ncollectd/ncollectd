c9e4c753109a6c934cba273b3a3f8d856fcb3d44
Manuel Luis Sanmartín Rozada <manuel.luis@gmail.com>
2024-03-19 23:01:57 +0100

    Plugin conntrack: migrate plugin

--- src/plugins/conntrack/conntrack.c
+++ src/plugins/conntrack/conntrack.c
@@ -1,96 +1,130 @@
-/**
- * collectd - src/conntrack.c
- * Copyright (C) 2009  Tomasz Pala
- *
- * This program is free software; you can redistribute it and/or modify it
- * under the terms of the GNU General Public License as published by the
- * Free Software Foundation; only version 2 of the License is applicable.
- *
- * This program is distributed in the hope that it will be useful, but
- * WITHOUT ANY WARRANTY; without even the implied warranty of
- * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.	 See the GNU
- * General Public License for more details.
- *
- * You should have received a copy of the GNU General Public License along
- * with this program; if not, write to the Free Software Foundation, Inc.,
- * 51 Franklin St, Fifth Floor, Boston, MA  02110-1301 USA
- *
- * Authors:
- *   Tomasz Pala <gotar at pld-linux.org>
- * based on entropy.c by:
- *   Florian octo Forster <octo at collectd.org>
- **/
+// SPDX-License-Identifier: GPL-2.0-only
+// SPDX-FileCopyrightText: Copyright (C) 2009 Tomasz Pala
+// SPDX-FileCopyrightText: Copyright (C) 2022-2024 Manuel Sanmartín
+// SPDX-FileContributor: Tomasz Pala <gotar at pld-linux.org>
+// SPDX-FileContributor: Manuel Sanmartín <manuel.luis at gmail.com>
 
-#include "collectd.h"
+// based on entropy.c by:
+//   Florian octo Forster <octo at collectd.org>
 
 #include "plugin.h"
-#include "utils/common/common.h"
+#include "libutils/common.h"
 
-#if !KERNEL_LINUX
+#ifndef KERNEL_LINUX
 #error "No applicable input method."
 #endif
 
-#define CONNTRACK_FILE "/proc/sys/net/netfilter/nf_conntrack_count"
-#define CONNTRACK_MAX_FILE "/proc/sys/net/netfilter/nf_conntrack_max"
-#define CONNTRACK_FILE_OLD "/proc/sys/net/ipv4/netfilter/ip_conntrack_count"
-#define CONNTRACK_MAX_FILE_OLD "/proc/sys/net/ipv4/netfilter/ip_conntrack_max"
+enum {
+    FAM_CONNTRACK_USED,
+    FAM_CONNTRACK_MAX,
+    FAM_CONNTRACK_NF,
+};
 
-static const char *config_keys[] = {"OldFiles"};
-static int config_keys_num = STATIC_ARRAY_SIZE(config_keys);
-/*
-    Each table/chain combo that will be queried goes into this list
-*/
+static metric_family_t fams[FAM_CONNTRACK_NF] = {
+    [FAM_CONNTRACK_USED] = {
+        .name = "system_conntrack_used",
+        .type = METRIC_TYPE_GAUGE,
+        .help = "Number of currently allocated flow entries.",
+    },
+    [FAM_CONNTRACK_MAX] = {
+        .name = "system_conntrack_max",
+        .type = METRIC_TYPE_GAUGE,
+        .help = "Size of connection tracking table.",
+    },
+};
 
-static int old_files;
+static char *conntrack_path;
+static char *conntrack_file;
+static char *conntrack_old_file;
 
-static int conntrack_config(const char *key,
-                            __attribute__((unused)) const char *value) {
-  if (strcmp(key, "OldFiles") == 0)
-    old_files = 1;
+static char *conntrack_max_path;
+static char *conntrack_max_file;
+static char *conntrack_max_old_file;
 
-  return 0;
+static int conntrack_read(void)
+{
+    if (conntrack_path == NULL) {
+        if (access(conntrack_file, R_OK) == 0) {
+            conntrack_path = conntrack_file;
+        } else if (access(conntrack_old_file, R_OK) == 0) {
+            conntrack_path = conntrack_old_file;
+        } else {
+            PLUGIN_ERROR("Not found nf_conntrack_count or ip_conntrack_count");
+            return -1;
+        }
+    }
+
+    double conntrack = 0;
+    if (parse_double_file(conntrack_path, &conntrack) != 0) {
+        PLUGIN_ERROR("Reading \"%s\" failed.", conntrack_path);
+        return -1;
+    }
+
+    if (conntrack_max_path == NULL) {
+        if (access(conntrack_max_file, R_OK) == 0) {
+            conntrack_max_path = conntrack_max_file;
+        } else if (access(conntrack_max_old_file, R_OK) == 0) {
+            conntrack_max_path = conntrack_max_old_file;
+        } else {
+            PLUGIN_ERROR("Not found nf_conntrack_max or ip_conntrack_max");
+            return -1;
+        }
+    }
+
+    double conntrack_max = 0;
+    if (parse_double_file(conntrack_max_path, &conntrack_max) != 0) {
+        PLUGIN_ERROR("Reading \"%s\" failed.", conntrack_max_path);
+        return -1;
+    }
+
+    metric_family_append(&fams[FAM_CONNTRACK_USED], VALUE_GAUGE(conntrack), NULL, NULL);
+    metric_family_append(&fams[FAM_CONNTRACK_MAX], VALUE_GAUGE(conntrack_max), NULL, NULL);
+
+    plugin_dispatch_metric_family_array(fams, FAM_CONNTRACK_NF, 0);
+    return 0;
 }
 
-static void conntrack_submit(const char *type, const char *type_instance,
-                             value_t conntrack) {
-  value_list_t vl = VALUE_LIST_INIT;
+static int conntrack_init(void)
+{
+    conntrack_file = plugin_procpath("sys/net/netfilter/nf_conntrack_count");
+    if (conntrack_file == NULL) {
+        PLUGIN_ERROR("Cannot get proc path.");
+        return -1;
+    }
 
-  vl.values = &conntrack;
-  vl.values_len = 1;
-  sstrncpy(vl.plugin, "conntrack", sizeof(vl.plugin));
-  sstrncpy(vl.type, type, sizeof(vl.type));
-  if (type_instance != NULL)
-    sstrncpy(vl.type_instance, type_instance, sizeof(vl.type_instance));
+    conntrack_old_file = plugin_procpath("sys/net/ipv4/netfilter/ip_conntrack_count");
+    if (conntrack_old_file == NULL) {
+        PLUGIN_ERROR("Cannot get proc path.");
+        return -1;
+    }
 
-  plugin_dispatch_values(&vl);
-} /* static void conntrack_submit */
+    conntrack_max_file = plugin_procpath("sys/net/netfilter/nf_conntrack_max");
+    if (conntrack_max_file == NULL) {
+        PLUGIN_ERROR("Cannot get proc path.");
+        return -1;
+    }
 
-static int conntrack_read(void) {
-  value_t conntrack, conntrack_max, conntrack_pct;
+    conntrack_max_old_file = plugin_procpath("sys/net/ipv4/netfilter/ip_conntrack_max");
+    if (conntrack_max_old_file == NULL) {
+        PLUGIN_ERROR("Cannot get proc path.");
+        return -1;
+    }
 
-  char const *path = old_files ? CONNTRACK_FILE_OLD : CONNTRACK_FILE;
-  if (parse_value_file(path, &conntrack, DS_TYPE_GAUGE) != 0) {
-    ERROR("conntrack plugin: Reading \"%s\" failed.", path);
-    return -1;
-  }
+    return 0;
+}
 
-  path = old_files ? CONNTRACK_MAX_FILE_OLD : CONNTRACK_MAX_FILE;
-  if (parse_value_file(path, &conntrack_max, DS_TYPE_GAUGE) != 0) {
-    ERROR("conntrack plugin: Reading \"%s\" failed.", path);
-    return -1;
-  }
+static int conntrack_shutdown(void)
+{
+    free(conntrack_file);
+    free(conntrack_old_file);
+    free(conntrack_max_file);
+    free(conntrack_max_old_file);
+    return 0;
+}
 
-  conntrack_pct.gauge = (conntrack.gauge / conntrack_max.gauge) * 100;
-
-  conntrack_submit("conntrack", NULL, conntrack);
-  conntrack_submit("conntrack", "max", conntrack_max);
-  conntrack_submit("percent", "used", conntrack_pct);
-
-  return 0;
-} /* static int conntrack_read */
-
-void module_register(void) {
-  plugin_register_config("conntrack", conntrack_config, config_keys,
-                         config_keys_num);
-  plugin_register_read("conntrack", conntrack_read);
-} /* void module_register */
+void module_register(void)
+{
+    plugin_register_init("conntrack", conntrack_init);
+    plugin_register_read("conntrack", conntrack_read);
+    plugin_register_shutdown("conntrack", conntrack_shutdown);
+}
