86cd092ff55e1d80b93a10b884a13fdb0c80c5c3
Manuel Luis Sanmartín Rozada <manuel.luis@gmail.com>
2024-03-19 23:02:04 +0100

    Plugin numa: add tests.

---
+++ src/plugins/numa/numa_test.c
@@ -0,0 +1,23 @@
+// SPDX-License-Identifier: GPL-2.0-only
+// SPDX-FileCopyrightText: Copyright (C) 2022-2024 Manuel Sanmartín
+// SPDX-FileContributor: Manuel Sanmartín <manuel.luis at gmail.com>
+
+#include "libtest/testing.h"
+
+extern void module_register(void);
+
+DEF_TEST(test01)
+{
+    EXPECT_EQ_INT(0, plugin_test_do_read(NULL, "src/plugins/numa/test01/sys", NULL,
+                                         "src/plugins/numa/test01/expect.txt"));
+    return 0;
+}
+
+int main(void)
+{
+    module_register();
+
+    RUN_TEST(test01);
+
+    END_TEST;
+}
