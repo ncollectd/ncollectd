553d49a2bf1607b462a4fa58b345db61c8505956
Manuel Luis Sanmartín Rozada <manuel.luis@gmail.com>
2024-03-19 23:02:02 +0100

    Plugin thermal: upgrade plugin.

--- src/plugins/thermal/thermal.c
+++ src/plugins/thermal/thermal.c
@@ -1,201 +1,227 @@
-/**
- * collectd - src/thermal.c
- * Copyright (C) 2008  Michał Mirosław
- *
- * This program is free software; you can redistribute it and/or modify it
- * under the terms of the GNU General Public License as published by the
- * Free Software Foundation; only version 2 of the License is applicable.
- *
- * This program is distributed in the hope that it will be useful, but
- * WITHOUT ANY WARRANTY; without even the implied warranty of
- * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU
- * General Public License for more details.
- *
- * You should have received a copy of the GNU General Public License along
- * with this program; if not, write to the Free Software Foundation, Inc.,
- * 51 Franklin St, Fifth Floor, Boston, MA  02110-1301 USA
- *
- * Authors:
- *   Michał Mirosław <mirq-linux at rere.qmqm.pl>
- **/
-
-#include "collectd.h"
+// SPDX-License-Identifier: GPL-2.0-only
+// SPDX-FileCopyrightText: Copyright (C) 2008 Michał Mirosław
+// SPDX-FileCopyrightText: Copyright (C) 2022-2024 Manuel Sanmartín
+// SPDX-FileContributor: Michał Mirosław <mirq-linux at rere.qmqm.pl>
+// SPDX-FileContributor: Manuel Sanmartín <manuel.luis at gmail.com>
 
 #include "plugin.h"
-#include "utils/common/common.h"
-#include "utils/ignorelist/ignorelist.h"
+#include "libutils/common.h"
+#include "libutils/exclist.h"
 
-#if !KERNEL_LINUX
+#ifndef KERNEL_LINUX
 #error "This module is for Linux only."
 #endif
 
-static const char *config_keys[] = {"Device", "IgnoreSelected",
-                                    "ForceUseProcfs"};
+enum {
+    FAM_COOLING_DEVICE_MAX_STATE = 0,
+    FAM_COOLING_DEVICE_CUR_STATE,
+    FAM_THERMAL_ZONE_CELSIUS,
+    FAM_THERMAL_MAX,
+};
 
-static const char *const dirname_sysfs = "/sys/class/thermal";
-static const char *const dirname_procfs = "/proc/acpi/thermal_zone";
+static metric_family_t fams[FAM_THERMAL_MAX] = {
+    [FAM_COOLING_DEVICE_MAX_STATE] = {
+        .name = "system_cooling_device_max_state",
+        .type = METRIC_TYPE_GAUGE,
+        .help = NULL,
+    },
+    [FAM_COOLING_DEVICE_CUR_STATE] = {
+        .name = "system_cooling_device_cur_state",
+        .type = METRIC_TYPE_GAUGE,
+        .help = NULL,
+    },
+    [FAM_THERMAL_ZONE_CELSIUS] = {
+        .name = "system_thermal_zone_celsius",
+        .type = METRIC_TYPE_GAUGE,
+        .help = NULL,
+    },
+};
+
+static char *path_sys_thermal;
+static char *path_proc_thermal_zone;
 
 static bool force_procfs;
-static ignorelist_t *device_list;
+static exclist_t excl_device;
+
+enum thermal_fs_e {
+    THERMAL_NONE = 0,
+    THERMAL_PROCFS,
+    THERMAL_SYSFS,
+};
+
+static enum thermal_fs_e thermal_fs;
 
 enum dev_type { TEMP = 0, COOLING_DEV };
 
-static void thermal_submit(const char *plugin_instance, enum dev_type dt,
-                           value_t value) {
-  value_list_t vl = VALUE_LIST_INIT;
-
-  vl.values = &value;
-  vl.values_len = 1;
-
-  sstrncpy(vl.plugin, "thermal", sizeof(vl.plugin));
-  if (plugin_instance != NULL)
-    sstrncpy(vl.plugin_instance, plugin_instance, sizeof(vl.plugin_instance));
-  sstrncpy(vl.type, (dt == TEMP) ? "temperature" : "gauge", sizeof(vl.type));
-
-  plugin_dispatch_values(&vl);
-}
-
-static int thermal_sysfs_device_read(const char __attribute__((unused)) * dir,
+static int thermal_sysfs_device_read(__attribute__((unused)) int dirfd,
+                                     const char __attribute__((unused)) * dir,
                                      const char *name,
-                                     void __attribute__((unused)) * user_data) {
-  char filename[PATH_MAX];
-  bool success = false;
-  value_t value;
+                                     __attribute__((unused)) void *user_data)
+{
+    char filename[PATH_MAX];
+    bool success = false;
 
-  if (device_list && ignorelist_match(device_list, name))
-    return -1;
+    if (!exclist_match(&excl_device, name))
+        return -1;
 
-  snprintf(filename, sizeof(filename), "%s/%s/temp", dirname_sysfs, name);
-  if (parse_value_file(filename, &value, DS_TYPE_GAUGE) == 0) {
-    value.gauge /= 1000.0;
-    thermal_submit(name, TEMP, value);
-    success = true;
-  }
+    snprintf(filename, sizeof(filename), "%s/%s/temp", path_sys_thermal, name);
+    double raw = 0;
+    if (parse_double_file(filename, &raw) == 0) {
+        metric_family_append(&fams[FAM_THERMAL_ZONE_CELSIUS], VALUE_GAUGE(raw/1000.0), NULL,
+                             &(label_pair_const_t){.name="zone", .value=name}, NULL);
+        success = true;
+    }
 
-  snprintf(filename, sizeof(filename), "%s/%s/cur_state", dirname_sysfs, name);
-  if (parse_value_file(filename, &value, DS_TYPE_GAUGE) == 0) {
-    thermal_submit(name, COOLING_DEV, value);
-    success = true;
-  }
+    snprintf(filename, sizeof(filename), "%s/%s/cur_state", path_sys_thermal, name);
+    if (parse_double_file(filename, &raw) == 0) {
+        metric_family_append(&fams[FAM_COOLING_DEVICE_CUR_STATE], VALUE_GAUGE(raw), NULL,
+                             &(label_pair_const_t){.name="device", .value=name}, NULL);
+        success = true;
+    }
 
-  return success ? 0 : -1;
+    snprintf(filename, sizeof(filename), "%s/%s/max_state", path_sys_thermal, name);
+    if (parse_double_file(filename, &raw) == 0) {
+        metric_family_append(&fams[FAM_COOLING_DEVICE_MAX_STATE], VALUE_GAUGE(raw), NULL,
+                             &(label_pair_const_t){.name="device", .value=name}, NULL);
+        success = true;
+    }
+
+    return success ? 0 : -1;
 }
 
-static int thermal_procfs_device_read(const char __attribute__((unused)) * dir,
+static int thermal_procfs_device_read(__attribute__((unused)) int dirfd,
+                                      const char __attribute__((unused)) * dir,
                                       const char *name,
-                                      void __attribute__((unused)) *
-                                          user_data) {
-  const char str_temp[] = "temperature:";
-  char filename[256];
-  char data[1024];
-  int len;
+                                      __attribute__((unused)) void *user_data)
+{
+    const char str_temp[] = "temperature:";
+    char filename[PATH_MAX];
+    char data[1024];
 
-  if (device_list && ignorelist_match(device_list, name))
-    return -1;
+    if (!exclist_match(&excl_device, name))
+        return -1;
 
-  /**
-   * rechot ~ # cat /proc/acpi/thermal_zone/THRM/temperature
-   * temperature:             55 C
-   */
+    /**
+     * rechot ~ # cat /proc/acpi/thermal_zone/THRM/temperature
+     * temperature:                         55 C
+     */
 
-  len = snprintf(filename, sizeof(filename), "%s/%s/temperature",
-                 dirname_procfs, name);
-  if ((len < 0) || ((size_t)len >= sizeof(filename)))
-    return -1;
+    int len = snprintf(filename, sizeof(filename), "%s/%s/temperature",
+                                                   path_proc_thermal_zone, name);
+    if ((len < 0) || ((size_t)len >= sizeof(filename)))
+        return -1;
 
-  len = (ssize_t)read_text_file_contents(filename, data, sizeof(data));
-  if ((len > 0) && ((size_t)len > sizeof(str_temp)) && (data[--len] == '\n') &&
-      (!strncmp(data, str_temp, sizeof(str_temp) - 1))) {
-    char *endptr = NULL;
-    double temp;
-    double factor, add;
+    len = (ssize_t)read_text_file_contents(filename, data, sizeof(data));
+    if ((len > 0) && ((size_t)len > sizeof(str_temp)) &&
+        (data[--len] == '\n') && (!strncmp(data, str_temp, sizeof(str_temp) - 1))) {
+        char *endptr = NULL;
+        double temp;
+        double factor, add;
 
-    if (data[--len] == 'C') {
-      add = 0;
-      factor = 1.0;
-    } else if (data[len] == 'F') {
-      add = -32;
-      factor = 5.0 / 9.0;
-    } else if (data[len] == 'K') {
-      add = -273.15;
-      factor = 1.0;
-    } else
-      return -1;
+        if (data[--len] == 'C') {
+            add = 0;
+            factor = 1.0;
+        } else if (data[len] == 'F') {
+            add = -32;
+            factor = 5.0 / 9.0;
+        } else if (data[len] == 'K') {
+            add = -273.15;
+            factor = 1.0;
+        } else
+            return -1;
 
-    while (len > 0 && data[--len] == ' ')
-      ;
-    data[len + 1] = 0;
+        while (len > 0 && data[--len] == ' ');
+        data[len + 1] = 0;
 
-    while (len > 0 && data[--len] != ' ')
-      ;
-    ++len;
+        while (len > 0 && data[--len] != ' ');
+        ++len;
 
-    errno = 0;
-    temp = (strtod(data + len, &endptr) + add) * factor;
+        errno = 0;
+        temp = (strtod(data + len, &endptr) + add) * factor;
 
-    if (endptr != data + len && errno == 0) {
-      thermal_submit(name, TEMP, (value_t){.gauge = temp});
-      return 0;
+        if (endptr != data + len && errno == 0) {
+            metric_family_append(&fams[FAM_THERMAL_ZONE_CELSIUS], VALUE_GAUGE(temp), NULL,
+                                 &(label_pair_const_t){.name="zone", .value=name}, NULL);
+            return 0;
+        }
     }
-  }
 
-  return -1;
-}
-
-static int thermal_config(const char *key, const char *value) {
-  if (device_list == NULL)
-    device_list = ignorelist_create(1);
-
-  if (strcasecmp(key, "Device") == 0) {
-    if (ignorelist_add(device_list, value)) {
-      ERROR("thermal plugin: "
-            "Cannot add value to ignorelist.");
-      return 1;
-    }
-  } else if (strcasecmp(key, "IgnoreSelected") == 0) {
-    ignorelist_set_invert(device_list, 1);
-    if (IS_TRUE(value))
-      ignorelist_set_invert(device_list, 0);
-  } else if (strcasecmp(key, "ForceUseProcfs") == 0) {
-    force_procfs = false;
-    if (IS_TRUE(value))
-      force_procfs = true;
-  } else {
     return -1;
-  }
-
-  return 0;
 }
 
-static int thermal_sysfs_read(void) {
-  return walk_directory(dirname_sysfs, thermal_sysfs_device_read, NULL, 0);
+static int thermal_read(void)
+{
+    if (thermal_fs == THERMAL_PROCFS) {
+        walk_directory(path_proc_thermal_zone, thermal_procfs_device_read, NULL, 0);
+    } else {
+        walk_directory(path_sys_thermal, thermal_sysfs_device_read, NULL, 0);
+    }
+
+    plugin_dispatch_metric_family_array(fams, FAM_THERMAL_MAX, 0);
+    return 0;
 }
 
-static int thermal_procfs_read(void) {
-  return walk_directory(dirname_procfs, thermal_procfs_device_read, NULL, 0);
+static int thermal_config(config_item_t *ci)
+{
+    int status = 0;
+
+    for (int i = 0; i < ci->children_num; i++) {
+        config_item_t *child = ci->children + i;
+        if (strcasecmp(child->key, "device") == 0) {
+            status = cf_util_exclist(child, &excl_device);
+        } else if (strcasecmp(child->key, "force-use-procfs") == 0) {
+            status = cf_util_get_boolean(child, &force_procfs);
+        } else {
+            PLUGIN_ERROR("Option '%s' in %s:%d is not allowed.",
+                          child->key, cf_get_file(child), cf_get_lineno(child));
+            status = -1;
+        }
+
+        if (status != 0)
+            return -1;
+    }
+
+    return 0;
 }
 
-static int thermal_init(void) {
-  int ret = -1;
+static int thermal_init(void)
+{
+    path_sys_thermal = plugin_syspath("class/thermal");
+    if (path_sys_thermal == NULL) {
+        PLUGIN_ERROR("Cannot get sys path.");
+        return -1;
+    }
 
-  if (!force_procfs && access(dirname_sysfs, R_OK | X_OK) == 0) {
-    ret = plugin_register_read("thermal", thermal_sysfs_read);
-  } else if (access(dirname_procfs, R_OK | X_OK) == 0) {
-    ret = plugin_register_read("thermal", thermal_procfs_read);
-  }
+    path_proc_thermal_zone = plugin_procpath("acpi/thermal_zone");
+    if (path_proc_thermal_zone == NULL) {
+        PLUGIN_ERROR("Cannot get proc path.");
+        return -1;
+    }
 
-  return ret;
+    int ret = -1;
+
+    if (!force_procfs && access(path_sys_thermal, R_OK | X_OK) == 0) {
+        thermal_fs = THERMAL_SYSFS;
+        ret = plugin_register_read("thermal", thermal_read);
+    } else if (access(path_proc_thermal_zone, R_OK | X_OK) == 0) {
+        thermal_fs = THERMAL_PROCFS;
+        ret = plugin_register_read("thermal", thermal_read);
+    }
+
+    return ret;
 }
 
-static int thermal_shutdown(void) {
-  ignorelist_free(device_list);
-
-  return 0;
+static int thermal_shutdown(void)
+{
+    free(path_sys_thermal);
+    free(path_proc_thermal_zone);
+    exclist_reset(&excl_device);
+    return 0;
 }
 
-void module_register(void) {
-  plugin_register_config("thermal", thermal_config, config_keys,
-                         STATIC_ARRAY_SIZE(config_keys));
-  plugin_register_init("thermal", thermal_init);
-  plugin_register_shutdown("thermal", thermal_shutdown);
+void module_register(void)
+{
+    plugin_register_config("thermal", thermal_config);
+    plugin_register_init("thermal", thermal_init);
+    plugin_register_shutdown("thermal", thermal_shutdown);
 }
