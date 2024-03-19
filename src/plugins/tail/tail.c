e28c591e47f6ec322ad60b27b3710dd8c3dc56a1
Manuel Luis Sanmartín Rozada <manuel.luis@gmail.com>
2024-03-19 23:02:02 +0100

    Plugin tail: upgrade plugin.

--- src/plugins/tail/tail.c
+++ src/plugins/tail/tail.c
@@ -1,300 +1,284 @@
-/**
- * collectd - src/tail.c
- * Copyright (C) 2008       Florian octo Forster
- *
- * Permission is hereby granted, free of charge, to any person obtaining a
- * copy of this software and associated documentation files (the "Software"),
- * to deal in the Software without restriction, including without limitation
- * the rights to use, copy, modify, merge, publish, distribute, sublicense,
- * and/or sell copies of the Software, and to permit persons to whom the
- * Software is furnished to do so, subject to the following conditions:
- *
- * The above copyright notice and this permission notice shall be included in
- * all copies or substantial portions of the Software.
- *
- * THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
- * IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
- * FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
- * AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
- * LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING
- * FROM, OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER
- * DEALINGS IN THE SOFTWARE.
- *
- * Authors:
- *   Florian octo Forster <octo at collectd.org>
- **/
-
-#include "collectd.h"
+// SPDX-License-Identifier: GPL-2.0-only or MIT
+// SPDX-FileCopyrightText: Copyright (C) 2007-2008 C-Ware, Inc.
+// SPDX-FileCopyrightText: Copyright (C) 2008-2013 Florian Forster
+// SPDX-FileCopyrightText: Copyright (C) 2013 Kris Nielander
+// SPDX-FileCopyrightText: Copyright (C) 2022-2024 Manuel Sanmartín
+// SPDX-FileContributor: Kris Nielander <nielander at fox-it.com>
+// SPDX-FileContributor: Florian Forster <octo at collectd.org>
+// SPDX-FileContributor: Luke Heberling <lukeh at c-ware.com>
+// SPDX-FileContributor: Manuel Sanmartín <manuel.luis at gmail.com>
 
 #include "plugin.h"
-#include "utils/common/common.h"
-#include "utils/latency/latency_config.h"
-#include "utils_tail_match.h"
+#include "libutils/common.h"
 
-/*
- *  <Plugin tail>
- *    <File "/var/log/exim4/mainlog">
- *      Plugin "mail"
- *      Instance "exim"
- *      Interval 60
- *	<Match>
- *	  Regex "S=([1-9][0-9]*)"
- *	  ExcludeRegex "U=root.*S="
- *	  DSType "CounterAdd"
- *	  Type "ipt_bytes"
- *	  Instance "total"
- *	</Match>
- *    </File>
- *  </Plugin>
- */
+#include <fcntl.h>
+#include <stdlib.h>
+#include <string.h>
+#include <sys/mman.h>
+#include <sys/stat.h>
 
-struct ctail_config_match_s {
-  char *regex;
-  char *excluderegex;
-  int flags;
-  char *type;
-  char *type_instance;
-  latency_config_t latency;
-};
-typedef struct ctail_config_match_s ctail_config_match_t;
+typedef struct {
+    char *file;
+    FILE *fh;
+    struct stat stat;
+    bool whole;
+    bool force_rewind;
+    plugin_match_t *matches;
+} tail_t;
 
-static size_t tail_file_num;
+static int tail_close(tail_t *tail)
+{
+    if (tail->fh == NULL)
+        return 0;
 
-static int ctail_read(user_data_t *ud);
+    fclose(tail->fh);
+    tail->fh = NULL;
+    return 0;
+}
 
-static void ctail_match_free(void *arg) {
-  tail_match_destroy((cu_tail_match_t *)arg);
-} /* void ctail_match_free */
+static int tail_reopen(tail_t *tail)
+{
+    struct stat stat_buf = {0};
+    /* coverity[TOCTOU] */
+    int status = stat(tail->file, &stat_buf);
+    if (status != 0) {
+        PLUGIN_ERROR("Stat '%s' failed: %s", tail->file, STRERRNO);
+        return -1;
+    }
 
-static int ctail_config_add_match_dstype(ctail_config_match_t *cm,
-                                         oconfig_item_t *ci) {
-  if ((ci->values_num != 1) || (ci->values[0].type != OCONFIG_TYPE_STRING)) {
-    WARNING("tail plugin: `DSType' needs exactly one string argument.");
-    return -1;
-  }
+    /* The file is already open.. */
+    if ((tail->fh != NULL) && (stat_buf.st_ino == tail->stat.st_ino)) {
+        /* Seek to the beginning if file was truncated */
+        if (stat_buf.st_size < tail->stat.st_size) {
+            PLUGIN_INFO("File '%s' was truncated.", tail->file);
+            status = fseek(tail->fh, 0, SEEK_SET);
+            if (status != 0) {
+                PLUGIN_ERROR("fseek '%s' failed: %s", tail->file, STRERRNO);
+                fclose(tail->fh);
+                tail->fh = NULL;
+                return -1;
+            }
+        }
+        memcpy(&tail->stat, &stat_buf, sizeof(struct stat));
+        return 1;
+    }
 
-  char const *ds_type = ci->values[0].value.string;
-  if (strncasecmp("Gauge", ds_type, strlen("Gauge")) == 0) {
-    cm->flags = UTILS_MATCH_DS_TYPE_GAUGE;
-    if (strcasecmp("GaugeAverage", ds_type) == 0)
-      cm->flags |= UTILS_MATCH_CF_GAUGE_AVERAGE;
-    else if (strcasecmp("GaugeMin", ds_type) == 0)
-      cm->flags |= UTILS_MATCH_CF_GAUGE_MIN;
-    else if (strcasecmp("GaugeMax", ds_type) == 0)
-      cm->flags |= UTILS_MATCH_CF_GAUGE_MAX;
-    else if (strcasecmp("GaugeLast", ds_type) == 0)
-      cm->flags |= UTILS_MATCH_CF_GAUGE_LAST;
-    else if (strcasecmp("GaugeInc", ds_type) == 0)
-      cm->flags |= UTILS_MATCH_CF_GAUGE_INC;
-    else if (strcasecmp("GaugeAdd", ds_type) == 0)
-      cm->flags |= UTILS_MATCH_CF_GAUGE_ADD;
-    else if (strcasecmp("GaugePersist", ci->values[0].value.string) == 0)
-      cm->flags |= UTILS_MATCH_CF_GAUGE_PERSIST;
-    else
-      cm->flags = 0;
-  } else if (strcasecmp("Distribution", ds_type) == 0) {
-    cm->flags = UTILS_MATCH_DS_TYPE_GAUGE | UTILS_MATCH_CF_GAUGE_DIST;
+    /* Unless flag for rewinding to the start is set, seek to the end
+     * if we re-open the same file again or the file opened is the first at all
+     * or the first after an error */
+    int seek_end = 0;
+    if ((tail->stat.st_ino == 0) || (tail->stat.st_ino == stat_buf.st_ino))
+        seek_end = !tail->force_rewind;
 
-    int status = latency_config(&cm->latency, ci);
+    FILE *fh = fopen(tail->file, "r");
+    if (fh == NULL) {
+        PLUGIN_ERROR("Cannot open '%s': %s", tail->file, STRERRNO);
+        return -1;
+    }
+
+    if (seek_end != 0) {
+        status = fseek(fh, 0, SEEK_END);
+        if (status != 0) {
+            PLUGIN_ERROR("fseek '%s' failed: %s", tail->file, STRERRNO);
+            fclose(fh);
+            return -1;
+        }
+    }
+
+    if (tail->fh != NULL)
+        fclose(tail->fh);
+    tail->fh = fh;
+    memcpy(&tail->stat, &stat_buf, sizeof(struct stat));
+
+    return 0;
+}
+
+static int tail_readline(tail_t *tail, char *buf, size_t buflen)
+{
+    if (tail->fh == NULL) {
+        int status = tail_reopen(tail);
+        if (status < 0)
+            return status;
+    }
+    assert(tail->fh != NULL);
+
+    /* Try to read from the filehandle. If that succeeds, everything appears to
+     * be fine and we can return. */
+    clearerr(tail->fh);
+    if (fgets(buf, buflen, tail->fh) != NULL) {
+        buf[buflen - 1] = '\0';
+        return 0;
+    }
+
+    /* Check if we encountered an error */
+    if (ferror(tail->fh) != 0) {
+        /* Jupp, error. Force `cu_tail_reopen' to reopen the file.. */
+        fclose(tail->fh);
+        tail->fh = NULL;
+    }
+    /* else: eof -> check if the file was moved away and reopen the new file if
+     * so.. */
+
+    int status = tail_reopen(tail);
+    /* error -> return with error */
+    if (status < 0) {
+        return status;
+    /* file end reached and file not reopened -> nothing more to read */
+    } else if (status > 0) {
+        buf[0] = 0;
+        return 0;
+    }
+
+    /* If we get here: file was re-opened and there may be more to read.. Let's
+     * try again. */
+    if (fgets(buf, buflen, tail->fh) != NULL) {
+        buf[buflen - 1] = '\0';
+        return 0;
+    }
+
+    if (ferror(tail->fh) != 0) {
+        PLUGIN_WARNING("fgets '%s' returned an error: %s", tail->file, STRERRNO);
+        fclose(tail->fh);
+        tail->fh = NULL;
+        return -1;
+    }
+
+    /* EOf, well, apparently the new file is empty.. */
+    buf[0] = 0;
+    return 0;
+}
+
+static int tail_read(user_data_t *ud)
+{
+    tail_t *tail = ud->data;
+
+    while (true) {
+        char buf[8192];
+        int status = tail_readline(tail, buf, sizeof(buf));
+        if (status != 0) {
+            PLUGIN_ERROR("File '%s': tail_readline failed with status %i.", tail->file, status);
+            return -1;
+        }
+
+        /* check for EOF */
+        if (buf[0] == 0)
+          break;
+
+        size_t len = strlen(buf);
+        while (len > 0) {
+            if (buf[len - 1] != '\n')
+                break;
+            buf[len - 1] = '\0';
+            len--;
+        }
+
+        if (len == 0)
+            continue;
+
+        status = plugin_match(tail->matches, buf);
+        if (status != 0)
+            PLUGIN_WARNING("plugin_match failed.");
+    }
+
+    if (tail->whole)
+        tail_close(tail);
+
+    plugin_match_dispatch(tail->matches, true);
+
+    return 0;
+}
+
+static void tail_destroy(void *arg)
+{
+    tail_t *tail = arg;
+    if (tail == NULL)
+        return;
+
+    if (tail->fh != NULL)
+        fclose(tail->fh);
+
+    free(tail->file);
+
+    if (tail->matches != NULL)
+        plugin_match_shutdown(tail->matches);
+
+    free(tail);
+    return;
+}
+
+static int tail_config_file(config_item_t *ci)
+{
+    cdtime_t interval = 0;
+
+    char *file = NULL;
+    int status = cf_util_get_string(ci, &file);
     if (status != 0)
-      return status;
-  } else if (strncasecmp("Counter", ds_type, strlen("Counter")) == 0) {
-    cm->flags = UTILS_MATCH_DS_TYPE_COUNTER;
-    if (strcasecmp("CounterSet", ds_type) == 0)
-      cm->flags |= UTILS_MATCH_CF_COUNTER_SET;
-    else if (strcasecmp("CounterAdd", ds_type) == 0)
-      cm->flags |= UTILS_MATCH_CF_COUNTER_ADD;
-    else if (strcasecmp("CounterInc", ds_type) == 0)
-      cm->flags |= UTILS_MATCH_CF_COUNTER_INC;
-    else
-      cm->flags = 0;
-  } else if (strncasecmp("Derive", ds_type, strlen("Derive")) == 0) {
-    cm->flags = UTILS_MATCH_DS_TYPE_DERIVE;
-    if (strcasecmp("DeriveSet", ds_type) == 0)
-      cm->flags |= UTILS_MATCH_CF_DERIVE_SET;
-    else if (strcasecmp("DeriveAdd", ds_type) == 0)
-      cm->flags |= UTILS_MATCH_CF_DERIVE_ADD;
-    else if (strcasecmp("DeriveInc", ds_type) == 0)
-      cm->flags |= UTILS_MATCH_CF_DERIVE_INC;
-    else
-      cm->flags = 0;
-  } else {
-    cm->flags = 0;
-  }
+        return status;
 
-  if (cm->flags == 0) {
-    WARNING("tail plugin: `%s' is not a valid argument to `DSType'.",
-            ci->values[0].value.string);
-    return -1;
-  }
+    tail_t *tail  = calloc(1, sizeof(*tail));
+    if (tail == NULL)
+        return -1;
+    tail->file = file;
+    tail->whole = false;
 
-  return 0;
-} /* int ctail_config_add_match_dstype */
+    for (int i = 0; i < ci->children_num; ++i) {
+        config_item_t *child = ci->children + i;
 
-static int ctail_config_add_match(cu_tail_match_t *tm, const char *plugin_name,
-                                  const char *plugin_instance,
-                                  oconfig_item_t *ci) {
-  ctail_config_match_t cm = {0};
-  int status;
+        if (strcasecmp("interval", child->key) == 0) {
+            status = cf_util_get_cdtime(child, &interval);
+        } else if (strcasecmp("whole", child->key) == 0) {
+            status = cf_util_get_boolean(child, &tail->whole);
+        } else if (strcasecmp("match", child->key) == 0) {
+            status = plugin_match_config(child, &tail->matches);
+        } else {
+            PLUGIN_ERROR("Option '%s' in %s:%d is not allowed.",
+                          child->key, cf_get_file(child), cf_get_lineno(child));
+            status = -1;
+        }
 
-  if (ci->values_num != 0) {
-    WARNING("tail plugin: Ignoring arguments for the `Match' block.");
-  }
-
-  status = 0;
-  for (int i = 0; i < ci->children_num; i++) {
-    oconfig_item_t *option = ci->children + i;
-
-    if (strcasecmp("Regex", option->key) == 0)
-      status = cf_util_get_string(option, &cm.regex);
-    else if (strcasecmp("ExcludeRegex", option->key) == 0)
-      status = cf_util_get_string(option, &cm.excluderegex);
-    else if (strcasecmp("DSType", option->key) == 0)
-      status = ctail_config_add_match_dstype(&cm, option);
-    else if (strcasecmp("Type", option->key) == 0)
-      status = cf_util_get_string(option, &cm.type);
-    else if (strcasecmp("Instance", option->key) == 0)
-      status = cf_util_get_string(option, &cm.type_instance);
-    else {
-      WARNING("tail plugin: Option `%s' not allowed here.", option->key);
-      status = -1;
+        if (status != 0)
+            break;
     }
 
-    if (status != 0)
-      break;
-  } /* for (i = 0; i < ci->children_num; i++) */
-
-  while (status == 0) {
-    if (cm.regex == NULL) {
-      WARNING("tail plugin: `Regex' missing in `Match' block.");
-      status = -1;
-      break;
+    if (status == 0) {
+        if (tail->matches == NULL) {
+            PLUGIN_ERROR("No (valid) 'match' block ");
+            status = -1;
+        }
     }
 
-    if (cm.type == NULL) {
-      WARNING("tail plugin: `Type' missing in `Match' block.");
-      status = -1;
-      break;
+    if (status != 0) {
+        tail_destroy(tail);
+        return -1;
     }
 
-    if (cm.flags == 0) {
-      WARNING("tail plugin: `DSType' missing in `Match' block.");
-      status = -1;
-      break;
-    }
+    return plugin_register_complex_read("tail", tail->file, tail_read, interval,
+                                        &(user_data_t){.data=tail, .free_func=tail_destroy});
+}
 
-    break;
-  } /* while (status == 0) */
-
-  if (status == 0) {
-    // TODO(octo): there's nothing "simple" about the latency stuff …
-    status = tail_match_add_match_simple(
-        tm, cm.regex, cm.excluderegex, cm.flags,
-        (plugin_name != NULL) ? plugin_name : "tail", plugin_instance, cm.type,
-        cm.type_instance, cm.latency);
-
-    if (status != 0)
-      ERROR("tail plugin: tail_match_add_match_simple failed.");
-  }
-
-  sfree(cm.regex);
-  sfree(cm.excluderegex);
-  sfree(cm.type);
-  sfree(cm.type_instance);
-  latency_config_free(cm.latency);
-
-  return status;
-} /* int ctail_config_add_match */
-
-static int ctail_config_add_file(oconfig_item_t *ci) {
-  cu_tail_match_t *tm;
-  cdtime_t interval = 0;
-  char *plugin_name = NULL;
-  char *plugin_instance = NULL;
-  int num_matches = 0;
-
-  if ((ci->values_num != 1) || (ci->values[0].type != OCONFIG_TYPE_STRING)) {
-    WARNING("tail plugin: `File' needs exactly one string argument.");
-    return -1;
-  }
-
-  tm = tail_match_create(ci->values[0].value.string);
-  if (tm == NULL) {
-    ERROR("tail plugin: tail_match_create (%s) failed.",
-          ci->values[0].value.string);
-    return -1;
-  }
-
-  for (int i = 0; i < ci->children_num; i++) {
-    oconfig_item_t *option = ci->children + i;
+static int tail_config(config_item_t *ci)
+{
     int status = 0;
 
-    if (strcasecmp("Plugin", option->key) == 0)
-      status = cf_util_get_string(option, &plugin_name);
-    else if (strcasecmp("Instance", option->key) == 0)
-      status = cf_util_get_string(option, &plugin_instance);
-    else if (strcasecmp("Interval", option->key) == 0)
-      cf_util_get_cdtime(option, &interval);
-    else if (strcasecmp("Match", option->key) == 0) {
-      status = ctail_config_add_match(tm, plugin_name, plugin_instance, option);
-      if (status == 0)
-        num_matches++;
-      /* Be mild with failed matches.. */
-      status = 0;
-    } else {
-      status = -1;
+    for (int i = 0; i < ci->children_num; ++i) {
+        config_item_t *child = ci->children + i;
+
+        if (strcasecmp("file", child->key) == 0) {
+            status = tail_config_file(child);
+        } else {
+            PLUGIN_ERROR("The configuration option '%s' in %s:%d is not allowed here.",
+                         child->key, cf_get_file(child), cf_get_lineno(child));
+            status = -1;
+        }
+
+        if (status != 0)
+            return -1;
     }
 
-    if (status != 0)
-      break;
-  } /* for (i = 0; i < ci->children_num; i++) */
+    return 0;
+}
 
-  sfree(plugin_name);
-  sfree(plugin_instance);
-
-  if (num_matches == 0) {
-    ERROR("tail plugin: No (valid) matches found for file `%s'.",
-          ci->values[0].value.string);
-    tail_match_destroy(tm);
-    return -1;
-  }
-
-  char str[255];
-  snprintf(str, sizeof(str), "tail-%zu", tail_file_num++);
-
-  plugin_register_complex_read(
-      NULL, str, ctail_read, interval,
-      &(user_data_t){.data = tm, .free_func = ctail_match_free});
-
-  return 0;
-} /* int ctail_config_add_file */
-
-static int ctail_config(oconfig_item_t *ci) {
-  for (int i = 0; i < ci->children_num; i++) {
-    oconfig_item_t *option = ci->children + i;
-
-    if (strcasecmp("File", option->key) == 0)
-      ctail_config_add_file(option);
-    else {
-      WARNING("tail plugin: Option `%s' not allowed here.", option->key);
-    }
-  } /* for (i = 0; i < ci->children_num; i++) */
-
-  return 0;
-} /* int ctail_config */
-
-static int ctail_read(user_data_t *ud) {
-  int status;
-
-  status = tail_match_read((cu_tail_match_t *)ud->data, 0);
-  if (status != 0) {
-    ERROR("tail plugin: tail_match_read failed.");
-    return -1;
-  }
-
-  return 0;
-} /* int ctail_read */
-
-void module_register(void) {
-  plugin_register_complex_config("tail", ctail_config);
-} /* void module_register */
+void module_register(void)
+{
+    plugin_register_config("tail", tail_config);
+}
