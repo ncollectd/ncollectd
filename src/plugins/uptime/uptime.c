f420b426e12be9b574bf2db3fef3c98583723785
Manuel Luis Sanmartín Rozada <manuel.luis@gmail.com>
2024-03-19 23:02:02 +0100

    Plugin uptime: upgrade plugin.

--- src/plugins/uptime/uptime.c
+++ src/plugins/uptime/uptime.c
@@ -1,84 +1,38 @@
-/**
- * collectd - src/uptime.c
- * Copyright (C) 2009	Marco Chiappero
- *
- * This program is free software; you can redistribute it and/or modify it
- * under the terms of the GNU General Public License as published by the
- * Free Software Foundation; only version 2 of the License is applicable.
- *
- * This program is distributed in the hope that it will be useful, but
- * WITHOUT ANY WARRANTY; without even the implied warranty of
- * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU
- * General Public License for more details.
- *
- * You should have received a copy of the GNU General Public License along
- * with this program; if not, write to the Free Software Foundation, Inc.,
- * 51 Franklin St, Fifth Floor, Boston, MA  02110-1301 USA
- *
- * Authors:
- *   Marco Chiappero <marco at absence.it>
- **/
-
-#include "collectd.h"
+// SPDX-License-Identifier: GPL-2.0-only
+// SPDX-FileCopyrightText: Copyright (C) 2009 Marco Chiappero
+// SPDX-FileCopyrightText: Copyright (C) 2022-2024 Manuel Sanmartín
+// SPDX-FileContributor: Marco Chiappero <marco at absence.it>
+// SPDX-FileContributor: Manuel Sanmartín <manuel.luis at gmail.com>
 
 #include "plugin.h"
-#include "utils/common/common.h"
+#include "libutils/common.h"
 
-#if KERNEL_LINUX
+#ifdef KERNEL_LINUX
 #include <sys/sysinfo.h>
-/* #endif KERNEL_LINUX */
-
-#elif HAVE_LIBKSTAT
-/* Using kstats chain to retrieve the boot time on Solaris / OpenSolaris systems
- */
-/* #endif HAVE_LIBKSTAT */
-
-#elif HAVE_SYS_SYSCTL_H
+#elif defined(HAVE_LIBKSTAT)
+/* Using kstats chain to retrieve the boot time on Solaris / OpenSolaris systems */
+#elif defined(HAVE_SYS_SYSCTL_H)
 #include <sys/sysctl.h>
-/* Using sysctl interface to retrieve the boot time on *BSD / Darwin / OS X
- * systems */
-/* #endif HAVE_SYS_SYSCTL_H */
-
-#elif HAVE_PERFSTAT
+/* Using sysctl interface to retrieve the boot time on *BSD / Darwin / OS X systems */
+#elif defined(HAVE_PERFSTAT)
 #include <libperfstat.h>
 #include <sys/protosw.h>
 /* Using perfstat_cpu_total to retrive the boot time in AIX */
-/* #endif HAVE_PERFSTAT */
-
 #else
 #error "No applicable input method."
 #endif
 
-/*
- * Global variables
- */
-
-#if HAVE_KSTAT_H
+#ifdef HAVE_KSTAT_H
 #include <kstat.h>
+static kstat_ctl_t *kc;
 #endif
 
-#if HAVE_LIBKSTAT
-extern kstat_ctl_t *kc;
-#endif /* #endif HAVE_LIBKSTAT */
 
-static void uptime_submit(gauge_t value) {
-  metric_family_t fam = {
-      .name = "uptime_seconds",
-      .type = METRIC_TYPE_GAUGE,
-  };
-
-  metric_family_metric_append(&fam, (metric_t){
-                                        .value.gauge = value,
-                                    });
-
-  int status = plugin_dispatch_metric_family(&fam);
-  if (status != 0) {
-    ERROR("uptime plugin: plugin_dispatch_metric_family failed: %s",
-          STRERROR(status));
-  }
-
-  metric_family_metric_reset(&fam);
-}
+static metric_family_t fam = {
+    .name = "system_uptime_seconds",
+    .type = METRIC_TYPE_GAUGE,
+    .help = "System uptime",
+};
 
 /*
  * On most unix systems the uptime is calculated by looking at the boot
@@ -90,121 +44,110 @@ static void uptime_submit(gauge_t value) {
  * the boot time, the plugin is unregistered and there is no chance to
  * try again later. Nevertheless, this is very unlikely to happen.
  */
-static time_t uptime_get_sys(void) { /* {{{ */
-  time_t result;
-#if KERNEL_LINUX
-  struct sysinfo info;
-  int status;
+static time_t uptime_get_sys(void)
+{
+    time_t result = 0;
 
-  status = sysinfo(&info);
-  if (status != 0) {
-    ERROR("uptime plugin: Error calling sysinfo: %s", STRERRNO);
-    return -1;
-  }
+#ifdef KERNEL_LINUX
+    struct sysinfo info;
 
-  result = (time_t)info.uptime;
-  /* #endif KERNEL_LINUX */
+    int status = sysinfo(&info);
+    if (unlikely(status != 0)) {
+        PLUGIN_ERROR("Error calling sysinfo: %s", STRERRNO);
+        return -1;
+    }
 
-#elif HAVE_LIBKSTAT
-  kstat_t *ksp;
-  kstat_named_t *knp;
+    result = (time_t)info.uptime;
 
-  ksp = NULL;
-  knp = NULL;
+#elif defined(HAVE_LIBKSTAT)
+    /* kstats chain already opened by update_kstat (using *kc), verify everything
+     * went fine. */
+    if (unlikely(kc == NULL)) {
+        PLUGIN_ERROR("kstat chain control structure not available.");
+        return -1;
+    }
 
-  /* kstats chain already opened by update_kstat (using *kc), verify everything
-   * went fine. */
-  if (kc == NULL) {
-    ERROR("uptime plugin: kstat chain control structure not available.");
-    return -1;
-  }
+    if (kstat_chain_update(kc) < 0) {
+        PLUGIN_ERROR("kstat_chain_update failed.");
+        return -1;
+    }
 
-  ksp = kstat_lookup(kc, "unix", 0, "system_misc");
-  if (ksp == NULL) {
-    ERROR("uptime plugin: Cannot find unix:0:system_misc kstat.");
-    return -1;
-  }
+    kstat_t *ksp = kstat_lookup(kc, "unix", 0, "system_misc");
+    if (unlikely(ksp == NULL)) {
+        PLUGIN_ERROR("Cannot find unix:0:system_misc kstat.");
+        return -1;
+    }
 
-  if (kstat_read(kc, ksp, NULL) < 0) {
-    ERROR("uptime plugin: kstat_read failed.");
-    return -1;
-  }
+    kid_t kcid = kstat_read(kc, ksp, NULL);
+    if (unlikely(kcid < 0)) {
+        PLUGIN_ERROR("kstat_read failed.");
+        return -1;
+    }
 
-  knp = (kstat_named_t *)kstat_data_lookup(ksp, "boot_time");
-  if (knp == NULL) {
-    ERROR("uptime plugin: kstat_data_lookup (boot_time) failed.");
-    return -1;
-  }
+    kstat_named_t *knp = (kstat_named_t *)kstat_data_lookup(ksp, "boot_time");
+    if (unlikely(knp == NULL)) {
+        PLUGIN_ERROR("kstat_data_lookup (boot_time) failed.");
+        return -1;
+    }
 
-  if (knp->value.ui32 == 0) {
-    ERROR("uptime plugin: kstat_data_lookup returned success, "
-          "but `boottime' is zero!");
-    return -1;
-  }
+    if (unlikely(knp->value.ui32 == 0)) {
+        PLUGIN_ERROR("kstat_data_lookup returned success, but `boottime' is zero!");
+        return -1;
+    }
 
-  result = time(NULL) - (time_t)knp->value.ui32;
-  /* #endif HAVE_LIBKSTAT */
+    result = time(NULL) - (time_t)knp->value.ui32;
 
-#elif HAVE_SYS_SYSCTL_H
-  struct timeval boottv = {0};
-  size_t boottv_len;
-  int status;
+#elif defined(HAVE_SYS_SYSCTL_H)
+    int mib[] = {CTL_KERN, KERN_BOOTTIME};
+    struct timeval boottv = {0};
+    size_t boottv_len = sizeof(boottv);
 
-  int mib[] = {CTL_KERN, KERN_BOOTTIME};
+    int status = sysctl(mib, STATIC_ARRAY_SIZE(mib), &boottv, &boottv_len,
+                         /* new_value = */ NULL, /* new_length = */ 0);
+    if (unlikely(status != 0)) {
+        PLUGIN_ERROR("No value read from sysctl interface: %s", STRERRNO);
+        return -1;
+    }
 
-  boottv_len = sizeof(boottv);
+    if (unlikely(boottv.tv_sec == 0)) {
+        PLUGIN_ERROR("sysctl(3) returned success, but `boottime' is zero!");
+        return -1;
+    }
 
-  status = sysctl(mib, STATIC_ARRAY_SIZE(mib), &boottv, &boottv_len,
-                  /* new_value = */ NULL, /* new_length = */ 0);
-  if (status != 0) {
-    ERROR("uptime plugin: No value read from sysctl interface: %s", STRERRNO);
-    return -1;
-  }
+    result = time(NULL) - boottv.tv_sec;
 
-  if (boottv.tv_sec == 0) {
-    ERROR("uptime plugin: sysctl(3) returned success, "
-          "but `boottime' is zero!");
-    return -1;
-  }
+#elif defined(HAVE_PERFSTAT)
+    perfstat_cpu_total_t cputotal;
 
-  result = time(NULL) - boottv.tv_sec;
-  /* #endif HAVE_SYS_SYSCTL_H */
+    int status = perfstat_cpu_total(NULL, &cputotal, sizeof(perfstat_cpu_total_t), 1);
+    if (unlikely(status < 0)) {
+        PLUGIN_ERROR("perfstat_cpu_total: %s", STRERRNO);
+        return -1;
+    }
 
-#elif HAVE_PERFSTAT
-  int status;
-  perfstat_cpu_total_t cputotal;
-  int hertz;
+    int hertz = sysconf(_SC_CLK_TCK);
+    if (hertz <= 0)
+        hertz = HZ;
 
-  status = perfstat_cpu_total(NULL, &cputotal, sizeof(perfstat_cpu_total_t), 1);
-  if (status < 0) {
-    ERROR("uptime plugin: perfstat_cpu_total: %s", STRERRNO);
-    return -1;
-  }
+    result = cputotal.lbolt / hertz;
+#endif
 
-  hertz = sysconf(_SC_CLK_TCK);
-  if (hertz <= 0)
-    hertz = HZ;
-
-  result = cputotal.lbolt / hertz;
-#endif /* HAVE_PERFSTAT */
-
-  return result;
-} /* }}} int uptime_get_sys */
-
-static int uptime_read(void) {
-  gauge_t uptime;
-  time_t elapsed;
-
-  /* calculate the amount of time elapsed since boot, AKA uptime */
-  elapsed = uptime_get_sys();
-
-  uptime = (gauge_t)elapsed;
-
-  uptime_submit(uptime);
-
-  return 0;
+    return result;
 }
 
-void module_register(void) {
-  plugin_register_read("uptime", uptime_read);
-} /* void module_register */
+static int uptime_read(void)
+{
+    /* calculate the amount of time elapsed since boot, AKA uptime */
+    time_t elapsed = uptime_get_sys();
+
+    metric_family_append(&fam, VALUE_GAUGE(elapsed), NULL, NULL);
+
+    plugin_dispatch_metric_family(&fam, 0);
+
+    return 0;
+}
+
+void module_register(void)
+{
+    plugin_register_read("uptime", uptime_read);
+}
