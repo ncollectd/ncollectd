b7b4cdb857b0c604c20650d6f50133ac709f972c
Manuel Luis Sanmartín Rozada <manuel.luis@gmail.com>
2024-03-19 23:02:00 +0100

    Plugin numa: upgrade plugin.

--- src/plugins/numa/numa.c
+++ src/plugins/numa/numa.c
@@ -1,151 +1,190 @@
-/**
- * collectd - src/numa.c
- * Copyright (C) 2012       Florian Forster
- *
- * Permission is hereby granted, free of charge, to any person obtaining a
- * copy of this software and associated documentation files (the "Software"),
- * to deal in the Software without restriction, including without limitation
- * the rights to use, copy, modify, merge, publish, distribute, sublicense,
- * and/or sell copies of the Software, and to permit persons to whom the
- * Software is furnished to do so, subject to the following conditions:
- *
- * The above copyright notice and this permission notice shall be included in
- * all copies or substantial portions of the Software.
- *
- * THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
- * IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
- * FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
- * AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
- * LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING
- * FROM, OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER
- * DEALINGS IN THE SOFTWARE.
- *
- * Authors:
- *   Florian Forster <octo at collectd.org>
- **/
-
-#include "collectd.h"
+// SPDX-License-Identifier: GPL-2.0-only OR MIT
+// SPDX-FileCopyrightText: Copyright (C) 2012 Florian Forster
+// SPDX-FileCopyrightText: Copyright (C) 2022-2024 Manuel Sanmartín
+// SPDX-FileContributor: Florian Forster <octo at collectd.org>
+// SPDX-FileContributor: Manuel Sanmartín <manuel.luis at gmail.com>
 
 #include "plugin.h"
-#include "utils/common/common.h"
+#include "libutils/common.h"
 
-#if !KERNEL_LINUX
+#ifndef KERNEL_LINUX
 #error "No applicable input method."
 #endif
 
-#ifndef NUMA_ROOT_DIR
-#define NUMA_ROOT_DIR "/sys/devices/system/node"
-#endif
+static char *path_sys_node;
 
 static int max_node = -1;
 
-static void numa_dispatch_value(int node, /* {{{ */
-                                const char *type_instance, value_t v) {
-  value_list_t vl = VALUE_LIST_INIT;
+enum {
+    FAM_NUMA_HIT,
+    FAM_NUMA_MISS,
+    FAM_NUMA_FOREIGN,
+    FAM_NUMA_LOCAL_NODE,
+    FAM_NUMA_OTHER_NODE,
+    FAM_NUMA_INTERLEAVE_HIT,
+    FAM_NUMA_MAX
+};
 
-  vl.values = &v;
-  vl.values_len = 1;
+static metric_family_t fams[FAM_NUMA_MAX] = {
+    [FAM_NUMA_HIT] = {
+        .name = "system_numa_hit",
+        .type = METRIC_TYPE_COUNTER,
+        .help = "The number of pages that were successfully allocated to this node.",
+    },
+    [FAM_NUMA_MISS] = {
+        .name = "system_numa_miss",
+        .type = METRIC_TYPE_COUNTER,
+        .help = "The number of pages that were allocated on this node "
+                "because of low memory on the intended node.",
+    },
+    [FAM_NUMA_FOREIGN] = {
+        .name = "system_numa_foreign",
+        .type = METRIC_TYPE_COUNTER,
+        .help = "The number of pages initially intended for this node "
+                "that were allocated to another node instead.",
+    },
+    [FAM_NUMA_LOCAL_NODE] = {
+        .name = "system_numa_local_node",
+        .type = METRIC_TYPE_COUNTER,
+        .help = "The number of pages successfully allocated on this node, "
+                "by a process on this node.",
+    },
+    [FAM_NUMA_OTHER_NODE] = {
+        .name = "system_numa_other_node",
+        .type = METRIC_TYPE_COUNTER,
+        .help = "The number of pages allocated on this node, by a process on another node.",
+    },
+    [FAM_NUMA_INTERLEAVE_HIT] = {
+        .name = "system_numa_interleave_hit",
+        .type = METRIC_TYPE_COUNTER,
+        .help = "The number of interleave policy pages successfully allocated to this node.",
+    },
+};
 
-  sstrncpy(vl.plugin, "numa", sizeof(vl.plugin));
-  snprintf(vl.plugin_instance, sizeof(vl.plugin_instance), "node%i", node);
-  sstrncpy(vl.type, "vmpage_action", sizeof(vl.type));
-  sstrncpy(vl.type_instance, type_instance, sizeof(vl.type_instance));
-
-  plugin_dispatch_values(&vl);
-} /* }}} void numa_dispatch_value */
-
-static int numa_read_node(int node) /* {{{ */
+static int numa_read_node(int node)
 {
-  char path[PATH_MAX];
-  FILE *fh;
-  char buffer[128];
-  int status;
-  int success;
-
-  snprintf(path, sizeof(path), NUMA_ROOT_DIR "/node%i/numastat", node);
-
-  fh = fopen(path, "r");
-  if (fh == NULL) {
-    ERROR("numa plugin: Reading node %i failed: open(%s): %s", node, path,
-          STRERRNO);
-    return -1;
-  }
-
-  success = 0;
-  while (fgets(buffer, sizeof(buffer), fh) != NULL) {
-    char *fields[4];
-    value_t v;
-
-    status = strsplit(buffer, fields, STATIC_ARRAY_SIZE(fields));
-    if (status != 2) {
-      WARNING("numa plugin: Ignoring line with unexpected "
-              "number of fields (node %i).",
-              node);
-      continue;
-    }
-
-    v.derive = 0;
-    status = parse_value(fields[1], &v, DS_TYPE_DERIVE);
-    if (status != 0)
-      continue;
-
-    numa_dispatch_value(node, fields[0], v);
-    success++;
-  }
-
-  fclose(fh);
-  return success ? 0 : -1;
-} /* }}} int numa_read_node */
-
-static int numa_read(void) /* {{{ */
-{
-  int i;
-  int status;
-  int success;
-
-  if (max_node < 0) {
-    WARNING("numa plugin: No NUMA nodes were detected.");
-    return -1;
-  }
-
-  success = 0;
-  for (i = 0; i <= max_node; i++) {
-    status = numa_read_node(i);
-    if (status == 0)
-      success++;
-  }
-
-  return success ? 0 : -1;
-} /* }}} int numa_read */
-
-static int numa_init(void) /* {{{ */
-{
-  /* Determine the number of nodes on this machine. */
-  while (42) {
     char path[PATH_MAX];
-    struct stat statbuf = {0};
-    int status;
+    snprintf(path, sizeof(path), "%s/node%i/numastat", path_sys_node, node);
 
-    snprintf(path, sizeof(path), NUMA_ROOT_DIR "/node%i", max_node + 1);
-
-    status = stat(path, &statbuf);
-    if (status == 0) {
-      max_node++;
-      continue;
-    } else if (errno == ENOENT) {
-      break;
-    } else /* ((status != 0) && (errno != ENOENT)) */
-    {
-      ERROR("numa plugin: stat(%s) failed: %s", path, STRERRNO);
-      return -1;
+    FILE *fh = fopen(path, "r");
+    if (fh == NULL) {
+        PLUGIN_ERROR("Reading node %i failed: open(%s): %s", node, path, STRERRNO);
+        return -1;
     }
-  }
 
-  DEBUG("numa plugin: Found %i nodes.", max_node + 1);
-  return 0;
-} /* }}} int numa_init */
+    char buffer[128];
+    int success = 0;
+    while (fgets(buffer, sizeof(buffer), fh) != NULL) {
+        char *fields[4];
 
-void module_register(void) {
-  plugin_register_init("numa", numa_init);
-  plugin_register_read("numa", numa_read);
-} /* void module_register */
+        int status = strsplit(buffer, fields, STATIC_ARRAY_SIZE(fields));
+        if (status != 2) {
+            PLUGIN_WARNING("Ignoring line with unexpected number of fields (node %i).", node);
+            continue;
+        }
+
+        uint64_t v;
+        status = parse_uinteger(fields[1], &v);
+        if (status != 0)
+            continue;
+
+        value_t value = VALUE_COUNTER(v);
+
+        char node_buffer[21];
+        snprintf(node_buffer, sizeof(node_buffer), "%i", node);
+
+        if (!strcmp(fields[0], "numa_hit")) {
+            metric_family_append(&fams[FAM_NUMA_HIT], value, NULL,
+                                 &(label_pair_const_t){.name="node", .value=node_buffer}, NULL);
+            success++;
+        } else if (!strcmp(fields[0], "numa_miss")) {
+            metric_family_append(&fams[FAM_NUMA_MISS], value, NULL,
+                                 &(label_pair_const_t){.name="node", .value=node_buffer}, NULL);
+            success++;
+        } else if (!strcmp(fields[0], "numa_foreign")) {
+            metric_family_append(&fams[FAM_NUMA_FOREIGN], value, NULL,
+                                 &(label_pair_const_t){.name="node", .value=node_buffer}, NULL);
+            success++;
+        } else if (!strcmp(fields[0], "local_node")) {
+            metric_family_append(&fams[FAM_NUMA_LOCAL_NODE], value, NULL,
+                                 &(label_pair_const_t){.name="node", .value=node_buffer}, NULL);
+            success++;
+        } else if (!strcmp(fields[0], "other_node")) {
+            metric_family_append(&fams[FAM_NUMA_OTHER_NODE], value, NULL,
+                                 &(label_pair_const_t){.name="node", .value=node_buffer}, NULL);
+            success++;
+        } else if (!strcmp(fields[0], "interleave_hit")) {
+            metric_family_append(&fams[FAM_NUMA_INTERLEAVE_HIT], value, NULL,
+                                 &(label_pair_const_t){.name="node", .value=node_buffer}, NULL);
+            success++;
+        }
+    }
+
+    fclose(fh);
+    return success ? 0 : -1;
+}
+
+static int numa_read(void)
+{
+    if (max_node < 0) {
+        PLUGIN_WARNING("No NUMA nodes were detected.");
+        return -1;
+    }
+
+    int success = 0;
+    for (int i = 0; i <= max_node; i++) {
+        int status = numa_read_node(i);
+        if (status == 0)
+            success++;
+    }
+
+    plugin_dispatch_metric_family_array(fams, FAM_NUMA_MAX, 0);
+
+    return success ? 0 : -1;
+}
+
+static int numa_init(void)
+{
+    path_sys_node = plugin_syspath("devices/system/node");
+    if (path_sys_node == NULL) {
+        PLUGIN_ERROR("Cannot get sys path.");
+        return -1;
+    }
+
+    /* Determine the number of nodes on this machine. */
+    while (true) {
+        char path[PATH_MAX];
+        struct stat statbuf = {0};
+        int status;
+
+        snprintf(path, sizeof(path), "%s/node%i", path_sys_node, max_node + 1);
+
+        status = stat(path, &statbuf);
+        if (status == 0) {
+            max_node++;
+            continue;
+        } else if (errno == ENOENT) {
+            break;
+        } else { /* ((status != 0) && (errno != ENOENT)) */
+            PLUGIN_ERROR("stat(%s) failed: %s", path, STRERRNO);
+            return -1;
+        }
+    }
+
+    PLUGIN_DEBUG("Found %i nodes.", max_node + 1);
+    return 0;
+}
+
+static int numa_shutdown(void)
+{
+    free(path_sys_node);
+    return 0;
+}
+
+void module_register(void)
+{
+    plugin_register_init("numa", numa_init);
+    plugin_register_init("numa", numa_init);
+    plugin_register_read("numa", numa_read);
+    plugin_register_shutdown("numa", numa_shutdown);
+}
