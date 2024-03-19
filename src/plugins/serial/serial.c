0e3c101d0a4dd86c83cdcb4f1b89626a1d83813a
Manuel Luis Sanmartín Rozada <manuel.luis@gmail.com>
2024-03-19 23:02:01 +0100

    Plugin serial: upgrade plugin.

--- src/plugins/serial/serial.c
+++ src/plugins/serial/serial.c
@@ -1,109 +1,182 @@
-/**
- * collectd - src/serial.c
- * Copyright (C) 2005,2006  David Bacher
- *
- * This program is free software; you can redistribute it and/or modify it
- * under the terms of the GNU General Public License as published by the
- * Free Software Foundation; either version 2 of the License, or (at your
- * option) any later version.
- *
- * This program is distributed in the hope that it will be useful, but
- * WITHOUT ANY WARRANTY; without even the implied warranty of
- * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU
- * General Public License for more details.
- *
- * You should have received a copy of the GNU General Public License along
- * with this program; if not, write to the Free Software Foundation, Inc.,
- * 51 Franklin St, Fifth Floor, Boston, MA  02110-1301 USA
- *
- * Authors:
- *   David Bacher <drbacher at gmail.com>
- *   Florian octo Forster <octo at collectd.org>
- **/
-
-#include "collectd.h"
+// SPDX-License-Identifier: GPL-2.0-or-later
+// SPDX-FileCopyrightText: Copyright (C) 2005,2006 David Bacher
+// SPDX-FileCopyrightText: Copyright (C) 2022-2024 Manuel Sanmartín
+// SPDX-FileContributor: David Bacher <drbacher at gmail.com>
+// SPDX-FileContributor: Florian octo Forster <octo at collectd.org>
+// SPDX-FileContributor: Manuel Sanmartín <manuel.luis at gmail.com>
 
 #include "plugin.h"
-#include "utils/common/common.h"
+#include "libutils/common.h"
 
-#if !KERNEL_LINUX
+#ifndef KERNEL_LINUX
 #error "No applicable input method."
 #endif
 
-static void serial_submit(const char *type_instance, derive_t rx, derive_t tx) {
-  value_list_t vl = VALUE_LIST_INIT;
-  value_t values[] = {
-      {.derive = rx},
-      {.derive = tx},
-  };
+static char *path_proc_serial;
 
-  vl.values = values;
-  vl.values_len = STATIC_ARRAY_SIZE(values);
-  sstrncpy(vl.plugin, "serial", sizeof(vl.plugin));
-  sstrncpy(vl.type, "serial_octets", sizeof(vl.type));
-  sstrncpy(vl.type_instance, type_instance, sizeof(vl.type_instance));
+enum {
+    FAM_SERIAL_READ,
+    FAM_SERIAL_WRITE,
+    FAM_SERIAL_FRAMING_ERRORS,
+    FAM_SERIAL_PARITY_ERRORS,
+    FAM_SERIAL_BREAK_CONDITIONS,
+    FAM_SERIAL_OVERRUN_ERRORS,
+    FAM_SERIAL_MAX,
+};
 
-  plugin_dispatch_values(&vl);
-}
+static metric_family_t fams[FAM_SERIAL_MAX] = {
+    [FAM_SERIAL_READ] = {
+        .name = "system_serial_read_bytes",
+        .type = METRIC_TYPE_COUNTER,
+        .help = "Total bytes read in serial port",
+    },
+    [FAM_SERIAL_WRITE] = {
+        .name = "system_serial_write_bytes",
+        .type = METRIC_TYPE_COUNTER,
+        .help = "Total bytes written in serial port",
+    },
+    [FAM_SERIAL_FRAMING_ERRORS] = {
+        .name = "system_serial_framing_errors",
+        .type = METRIC_TYPE_COUNTER,
+        .help = "Total framing errors (stop bit not found) in serial port",
+    },
+    [FAM_SERIAL_PARITY_ERRORS] = {
+        .name = "system_serial_parity_errors",
+        .type = METRIC_TYPE_COUNTER,
+        .help = "Total parity errors in serial port",
+    },
+    [FAM_SERIAL_BREAK_CONDITIONS] = {
+        .name = "system_serial_break_conditions",
+        .type = METRIC_TYPE_COUNTER,
+        .help = "Total break conditions in serial port",
+    },
+    [FAM_SERIAL_OVERRUN_ERRORS] = {
+        .name = "system_serial_overrun_errors",
+        .type = METRIC_TYPE_COUNTER,
+        .help = "Total receiver overrun errors in serial port",
+    },
+};
 
-static int serial_read(void) {
-  FILE *fh;
-  char buffer[1024];
-
-  /* there are a variety of names for the serial device */
-  if ((fh = fopen("/proc/tty/driver/serial", "r")) == NULL &&
-      (fh = fopen("/proc/tty/driver/ttyS", "r")) == NULL) {
-    WARNING("serial: fopen: %s", STRERRNO);
-    return -1;
-  }
-
-  while (fgets(buffer, sizeof(buffer), fh) != NULL) {
-    derive_t rx = 0;
-    derive_t tx = 0;
-    bool have_rx = false;
-    bool have_tx = false;
-    size_t len;
-
-    char *fields[16];
-    int numfields;
-
-    numfields = strsplit(buffer, fields, STATIC_ARRAY_SIZE(fields));
-    if (numfields < 6)
-      continue;
-
-    /*
-     * 0: uart:16550A port:000003F8 irq:4 tx:0 rx:0
-     * 1: uart:16550A port:000002F8 irq:3 tx:0 rx:0
-     */
-    len = strlen(fields[0]);
-    if (len < 2)
-      continue;
-    if (fields[0][len - 1] != ':')
-      continue;
-    fields[0][len - 1] = '\0';
-
-    for (int i = 1; i < numfields; i++) {
-      len = strlen(fields[i]);
-      if (len < 4)
-        continue;
-
-      if (strncmp(fields[i], "tx:", 3) == 0) {
-        if (strtoderive(fields[i] + 3, &tx) == 0)
-          have_tx = true;
-      } else if (strncmp(fields[i], "rx:", 3) == 0) {
-        if (strtoderive(fields[i] + 3, &rx) == 0)
-          have_rx = true;
-      }
+static int serial_read(void)
+{
+    FILE *fh = fopen(path_proc_serial, "r");
+    if (unlikely(fh == NULL)) {
+        PLUGIN_WARNING("Cannot open '%s': %s", path_proc_serial, STRERRNO);
+        return -1;
     }
 
-    if (have_rx && have_tx)
-      serial_submit(fields[0], rx, tx);
-  }
+    char buffer[1024];
+    while (fgets(buffer, sizeof(buffer), fh) != NULL) {
+        char *fields[16];
+        int numfields = strsplit(buffer, fields, STATIC_ARRAY_SIZE(fields));
+        if (numfields < 6)
+            continue;
 
-  fclose(fh);
-  return 0;
-} /* int serial_read */
+        /*
+         * 0: uart:16550A port:000003F8 irq:4 tx:0 rx:0
+         * 1: uart:16550A port:000002F8 irq:3 tx:0 rx:0
+         */
+        size_t len = strlen(fields[0]);
+        if (len < 2)
+            continue;
+        if (fields[0][len - 1] != ':')
+            continue;
+        fields[0][len - 1] = '\0';
 
-void module_register(void) {
-  plugin_register_read("serial", serial_read);
-} /* void module_register */
+        metric_t m = {0};
+        metric_label_set(&m, "line", fields[0]);
+
+        uint64_t value = 0;
+
+        for (int i = 1; i < numfields; i++) {
+            len = strlen(fields[i]);
+            if (len < 4)
+                continue;
+
+            switch (fields[i][0]) {
+            case 'i':
+                if (strncmp(fields[i], "irq:", 4) == 0)
+                    metric_label_set(&m, "irq", fields[i]+4);
+                break;
+            case 't':
+                if (strncmp(fields[i], "tx:", 3) == 0) {
+                    if (strtouint(fields[i] + 3, &value) == 0) {
+                        m.value = VALUE_COUNTER(value);
+                        metric_family_metric_append(&fams[FAM_SERIAL_READ], m);
+                    }
+                }
+                break;
+            case 'r':
+                if (strncmp(fields[i], "rx:", 3) == 0) {
+                    if (strtouint(fields[i] + 3, &value) == 0) {
+                         m.value = VALUE_COUNTER(value);
+                         metric_family_metric_append(&fams[FAM_SERIAL_WRITE], m);
+                    }
+                }
+                break;
+            case 'f':
+                if (strncmp(fields[i], "fe:", 3) == 0) {
+                    if (strtouint(fields[i] + 3, &value) == 0) {
+                        m.value = VALUE_COUNTER(value);
+                        metric_family_metric_append(&fams[FAM_SERIAL_FRAMING_ERRORS], m);
+                    }
+                }
+                break;
+            case 'p':
+                if (strncmp(fields[i], "pe:", 3) == 0) {
+                    if (strtouint(fields[i] + 3, &value) == 0) {
+                        m.value = VALUE_COUNTER(value);
+                        metric_family_metric_append(&fams[FAM_SERIAL_PARITY_ERRORS], m);
+                    }
+                }
+                break;
+            case 'b':
+                if (strncmp(fields[i], "brk:", 4) == 0) {
+                    if (strtouint(fields[i] + 4, &value) == 0) {
+                        m.value = VALUE_COUNTER(value);
+                        metric_family_metric_append(&fams[FAM_SERIAL_BREAK_CONDITIONS], m);
+                    }
+                }
+                break;
+            case 'o':
+                if (strncmp(fields[i], "oe:", 3) == 0) {
+                    if (strtouint(fields[i] + 3, &value) == 0) {
+                        m.value = VALUE_COUNTER(value);
+                        metric_family_metric_append(&fams[FAM_SERIAL_OVERRUN_ERRORS], m);
+                    }
+                }
+                break;
+            }
+        }
+
+        metric_reset(&m, METRIC_TYPE_COUNTER);
+    }
+    fclose(fh);
+
+    plugin_dispatch_metric_family_array(fams, FAM_SERIAL_MAX, 0);
+    return 0;
+}
+
+static int serial_init(void)
+{
+    path_proc_serial = plugin_procpath("tty/driver/serial");
+    if (path_proc_serial == NULL) {
+        PLUGIN_ERROR("Cannot get proc path.");
+        return -1;
+    }
+
+    return 0;
+}
+
+static int serial_shutdown(void)
+{
+    free(path_proc_serial);
+    return 0;
+}
+
+void module_register(void)
+{
+    plugin_register_init("serial", serial_init);
+    plugin_register_read("serial", serial_read);
+    plugin_register_shutdown("serial", serial_shutdown);
+}
