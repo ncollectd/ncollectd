6196d4b9c19b5b978e3d0fb2e80e37e2940fd5f8
Manuel Luis Sanmartín Rozada <manuel.luis@gmail.com>
2024-03-19 23:02:02 +0100

    Plugin ubi: upgrade plugin.

--- src/plugins/ubi/ubi.c
+++ src/plugins/ubi/ubi.c
@@ -1,147 +1,122 @@
-/**
- * collectd - src/ubi.c
- *
- * This program is free software; you can redistribute it and/or modify it
- * under the terms of the GNU General Public License as published by the
- * Free Software Foundation; only version 2 of the License is applicable.
- *
- * This program is distributed in the hope that it will be useful, but
- * WITHOUT ANY WARRANTY; without even the implied warranty of
- * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU
- * General Public License for more details.
- *
- * You should have received a copy of the GNU General Public License along
- * with this program; if not, write to the Free Software Foundation, Inc.,
- * 51 Franklin St, Fifth Floor, Boston, MA  02110-1301 USA
- */
-
-#include "collectd.h"
+// SPDX-License-Identifier: GPL-2.0-only
+// SPDX-FileCopyrightText: Copyright (C) 2022-2024 Manuel Sanmartín
+// SPDX-FileContributor: Manuel Sanmartín <manuel.luis at gmail.com>
 
 #include "plugin.h"
-#include "utils/common/common.h"
-#include "utils/ignorelist/ignorelist.h"
+#include "libutils/common.h"
+#include "libutils/exclist.h"
 
-#if !KERNEL_LINUX
+#ifndef KERNEL_LINUX
 #error "No applicable input method."
 #endif
 
-#define PLUGIN_NAME "ubi"
+static char *path_sys_class_ubi;
 
-#define SYS_PATH "/sys/class/ubi/"
-
-/*
- * Device attributes
- */
-#define DEV_BAD_COUNT                                                          \
-  "bad_peb_count" // Count of bad physical eraseblocks on the underlying MTD
-                  // device.
-#define MAXIMUM_ERASE "max_ec" // Current maximum erase counter value
-
-/*
- * The config key strings
- */
-#define DEVICE_KEY "Device"
-#define IGNORE_KEY "IgnoreSelected"
-
-static const char *config_keys[] = {
-    DEVICE_KEY,
-    IGNORE_KEY,
+enum {
+    FAM_UBI_BAD_PHYSICAL_ERASEBLOCKS,
+    FAM_UBI_MAXIMUM_PHYSICAL_ERASEBLOCKS,
+    FAM_UBI_MAX,
 };
-static int config_keys_num = STATIC_ARRAY_SIZE(config_keys);
 
-static ignorelist_t *ignorelist = NULL;
+static metric_family_t fams[FAM_UBI_MAX] = {
+    [FAM_UBI_BAD_PHYSICAL_ERASEBLOCKS] = {
+        .name = "system_ubi_bad_physical_eraseblocks",
+        .type = METRIC_TYPE_GAUGE,
+        .help = "Count of bad physical eraseblocks on the underlying MTD device.",
+    },
+    [FAM_UBI_MAXIMUM_PHYSICAL_ERASEBLOCKS] = {
+        .name = "system_ubi_maximum_physical_eraseblocks",
+        .type = METRIC_TYPE_GAUGE,
+        .help = "Maximum physical eraseblock erase counter value.",
+    },
+};
 
-/*
- * Private functions
- */
-static int ubi_config(const char *key, const char *value) {
-  if (ignorelist == NULL &&
-      (ignorelist = ignorelist_create(/* invert = */ 1)) == NULL)
-    return -1;
+static exclist_t excl_device;
 
-  if (strcasecmp(key, DEVICE_KEY) == 0)
-    ignorelist_add(ignorelist, value);
-  else if (strcasecmp(key, IGNORE_KEY) == 0)
-    ignorelist_set_invert(ignorelist, IS_TRUE(value) ? 0 : 1);
-  else
-    return -1;
+static int ubi_read_device(int dirfd, __attribute__((unused)) const char *path,
+                           const char *entry, __attribute__((unused)) void *ud)
+{
+    if (!exclist_match(&excl_device, entry))
+        return 0;
 
-  return 0;
-} /* int ubi_config */
+    int devfd = openat(dirfd, entry, O_RDONLY | O_DIRECTORY);
+    if (unlikely(devfd < 0))
+        return 0;
 
-static void ubi_submit(const char *dev_name, const char *type, gauge_t value) {
-  value_list_t vl = VALUE_LIST_INIT;
+    uint64_t value = 0;
 
-  if (ignorelist_match(ignorelist, dev_name) != 0)
-    return;
+    int status = filetouint_at(devfd, "bad_peb_count", &value);
+    if (unlikely(status != 0)) {
+        PLUGIN_ERROR("Did not find an integer in bad_peb_count");
+    } else {
+        metric_family_append(&fams[FAM_UBI_BAD_PHYSICAL_ERASEBLOCKS], VALUE_GAUGE(value), NULL,
+                             &(label_pair_const_t){.name="device", .value="entry"}, NULL);
+    }
 
-  vl.values = &(value_t){.gauge = value};
-  vl.values_len = 1;
-  sstrncpy(vl.plugin, PLUGIN_NAME, sizeof(vl.plugin));
-  sstrncpy(vl.type_instance, dev_name, sizeof(vl.type_instance));
-  sstrncpy(vl.type, type, sizeof(vl.type));
+    status = filetouint_at(devfd, "max_ec", &value);
+    if (unlikely(status != 0)) {
+        PLUGIN_ERROR("Did not find an integer in max_ec");
+    } else {
+        metric_family_append(&fams[FAM_UBI_MAXIMUM_PHYSICAL_ERASEBLOCKS], VALUE_GAUGE(value), NULL,
+                             &(label_pair_const_t){.name="device", .value="entry"}, NULL);
+    }
 
-  plugin_dispatch_values(&vl);
-} /* void ubi_submit */
+    close(devfd);
 
-static int ubi_read_dev_attr(const char *dev_name, const char *attr) {
-  FILE *f;
-  int val;
-  char
-      str[sizeof(SYS_PATH) + strlen(dev_name) + sizeof("/") + strlen(attr) + 1];
-  int n;
+    return 0;
+}
 
-  snprintf(str, sizeof(str), SYS_PATH "%s/%s", dev_name, attr);
+static int ubi_read(void)
+{
+    walk_directory(path_sys_class_ubi, ubi_read_device, NULL, 0);
 
-  if ((f = fopen(str, "r")) == NULL) {
-    ERROR(PLUGIN_NAME ": cannot open [%s]", str);
-    return -1;
-  }
+    plugin_dispatch_metric_family_array(fams, FAM_UBI_MAX, 0);
+    return 0;
+}
 
-  n = fscanf(f, "%d", &val);
-  fclose(f);
+static int ubi_config(config_item_t *ci)
+{
+    int status = 0;
 
-  if (n != 1) {
-    ERROR(PLUGIN_NAME " : did not find an integer in %s", str);
-    return -1;
-  }
+    for (int i = 0; i < ci->children_num; i++) {
+        config_item_t *child = ci->children + i;
+        if (strcasecmp(child->key, "device") == 0) {
+            status = cf_util_exclist(child, &excl_device);
+        } else {
+            PLUGIN_ERROR("Option '%s' in %s:%d is not allowed.",
+                          child->key, cf_get_file(child), cf_get_lineno(child));
+            status = -1;
+        }
 
-  ubi_submit(dev_name, attr, (gauge_t)val);
+        if (status != 0)
+            return -1;
+    }
 
-  return 0;
-} /* int ubi_read_dev_attr */
+    return 0;
+}
 
-static inline int ubi_read_dev_bad_count(const char *dev_name) {
-  return ubi_read_dev_attr(dev_name, DEV_BAD_COUNT);
-} /* int ubi_read_dev_bad_count */
+static int ubi_init(void)
+{
+    path_sys_class_ubi = plugin_procpath("class/ubi");
+    if (path_sys_class_ubi == NULL) {
+        PLUGIN_ERROR("Cannot get sys path.");
+        return -1;
+    }
 
-static inline int ubi_read_max_ec(const char *dev_name) {
-  return ubi_read_dev_attr(dev_name, MAXIMUM_ERASE);
-} /* int ubi_read_max_ec */
+    return 0;
+}
 
-static int ubi_read(void) {
-  DIR *dir;
-  struct dirent *dirent;
+static int ubi_shutdown(void)
+{
+    free(path_sys_class_ubi);
+    exclist_reset(&excl_device);
+    return 0;
+}
 
-  if ((dir = opendir(SYS_PATH)) == NULL) {
-    ERROR(PLUGIN_NAME " : cannot open dir " SYS_PATH);
-    return -1;
-  }
-
-  while ((dirent = readdir(dir)) != NULL) {
-    if (ignorelist_match(ignorelist, dirent->d_name))
-      continue;
-
-    ubi_read_dev_bad_count(dirent->d_name);
-    ubi_read_max_ec(dirent->d_name);
-  }
-
-  closedir(dir);
-
-  return 0;
-} /* int ubi_read */
-
-void module_register(void) {
-  plugin_register_config(PLUGIN_NAME, ubi_config, config_keys, config_keys_num);
-  plugin_register_read(PLUGIN_NAME, ubi_read);
-} /* void module_register */
+void module_register(void)
+{
+    plugin_register_init("ubi", ubi_init);
+    plugin_register_config("ubi", ubi_config);
+    plugin_register_read("ubi", ubi_read);
+    plugin_register_shutdown("ubi", ubi_shutdown);
+}
