01373cb4e322b2a1d4260213e59168f7a33d86c8
Manuel Luis Sanmartín Rozada <manuel.luis@gmail.com>
2024-05-15 11:58:34 +0200

    Plugin swap: add tests

---
+++ src/plugins/swap/swap_linux_test.c
@@ -0,0 +1,50 @@
+// SPDX-License-Identifier: GPL-2.0-only
+// SPDX-FileCopyrightText: Copyright (C) 2022-2024 Manuel Sanmartín
+// SPDX-FileContributor: Manuel Sanmartín <manuel.luis at gmail.com>
+
+#include "libtest/testing.h"
+
+extern void module_register(void);
+
+DEF_TEST(test01)
+{
+    EXPECT_EQ_INT(0, plugin_test_do_read("src/plugins/swap/test01/proc", NULL, NULL,
+                                         "src/plugins/swap/test01/expect.txt"));
+    return 0;
+}
+
+
+DEF_TEST(test02)
+{
+    config_item_t ci = (config_item_t) {
+        .key = "plugin",
+        .values_num = 1,
+        .values = (config_value_t[]) {
+            {.type = CONFIG_TYPE_STRING, .value.string ="swap"},
+        },
+        .children_num = 1,
+        .children = (config_item_t[]) {
+            {
+                .key = "report-by-device",
+                .values_num = 1,
+                .values = (config_value_t[]) {
+                    {.type = CONFIG_TYPE_BOOLEAN, .value.boolean = 1},
+                }
+            },
+        }
+    };
+
+    EXPECT_EQ_INT(0, plugin_test_do_read("src/plugins/swap/test02/proc", NULL, &ci,
+                                         "src/plugins/swap/test02/expect.txt"));
+    return 0;
+}
+
+int main(void)
+{
+    module_register();
+
+    RUN_TEST(test01);
+    RUN_TEST(test02);
+
+    END_TEST;
+}
