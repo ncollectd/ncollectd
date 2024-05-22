59d7d52cd0c94e2766bbf2ae8e42101812ffe775
Manuel Luis Sanmartín Rozada <manuel.luis@gmail.com>
2024-05-22 15:05:06 +0200

    Fix spaces in CheckStructHasMemberNoSizeOf.cmake

--- cmake/CheckStructHasMemberNoSizeOf.cmake
+++ cmake/CheckStructHasMemberNoSizeOf.cmake
@@ -4,20 +4,20 @@
 include(CheckSourceCompiles)
 
 macro (CHECK_STRUCT_HAS_MEMBER_NO_SIZEOF _STRUCT _MEMBER _HEADER _RESULT)
-  set(_INCLUDE_FILES)
-  foreach (it ${_HEADER})
-    string(APPEND _INCLUDE_FILES "#include <${it}>\n")
-  endforeach ()
+    set(_INCLUDE_FILES)
+    foreach (it ${_HEADER})
+        string(APPEND _INCLUDE_FILES "#include <${it}>\n")
+    endforeach ()
 
-  if("x${ARGN}" STREQUAL "x")
-    set(_lang C)
-  elseif("x${ARGN}" MATCHES "^xLANGUAGE;([a-zA-Z]+)$")
-    set(_lang "${CMAKE_MATCH_1}")
-  else()
-    message(FATAL_ERROR "Unknown arguments:\n  ${ARGN}\n")
-  endif()
+    if("x${ARGN}" STREQUAL "x")
+        set(_lang C)
+    elseif("x${ARGN}" MATCHES "^xLANGUAGE;([a-zA-Z]+)$")
+        set(_lang "${CMAKE_MATCH_1}")
+    else()
+        message(FATAL_ERROR "Unknown arguments:\n  ${ARGN}\n")
+    endif()
 
-  set(_CHECK_STRUCT_MEMBER_SOURCE_CODE "
+    set(_CHECK_STRUCT_MEMBER_SOURCE_CODE "
 ${_INCLUDE_FILES}
 int main()
 {
@@ -28,10 +28,10 @@ int main()
 ")
 
   if("${_lang}" STREQUAL "C")
-    CHECK_SOURCE_COMPILES(C "${_CHECK_STRUCT_MEMBER_SOURCE_CODE}" ${_RESULT})
+      CHECK_SOURCE_COMPILES(C "${_CHECK_STRUCT_MEMBER_SOURCE_CODE}" ${_RESULT})
   elseif("${_lang}" STREQUAL "CXX")
-    CHECK_SOURCE_COMPILES(CXX "${_CHECK_STRUCT_MEMBER_SOURCE_CODE}" ${_RESULT})
+      CHECK_SOURCE_COMPILES(CXX "${_CHECK_STRUCT_MEMBER_SOURCE_CODE}" ${_RESULT})
   else()
-    message(FATAL_ERROR "Unknown language:\n  ${_lang}\nSupported languages: C, CXX.\n")
+      message(FATAL_ERROR "Unknown language:\n  ${_lang}\nSupported languages: C, CXX.\n")
   endif()
 endmacro ()
