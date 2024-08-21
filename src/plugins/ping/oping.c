6629ceef136cd55c436f5c550da51c59455c4031
manuelluis <manuel.luis@gmail.com>
2024-08-21 13:58:08 +0200

    Change LGPL-2.1-or-later for GPL-2.1-or-later.

--- src/plugins/ping/oping.c
+++ src/plugins/ping/oping.c
@@ -1,4 +1,4 @@
-// SPDX-License-Identifier: LGPL-2.1-or-later
+// SPDX-License-Identifier: GPL-2.1-or-later
 // SPDX-FileCopyrightText: Copyright (C) 2006-2017 Florian octo Forster
 // SPDX-FileContributor: Florian octo Forster <ff at octo.it>
 
