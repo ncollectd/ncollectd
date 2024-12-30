3b1e4d34841a2a36225be43fed38baf1f0cf3788
Manuel Luis Sanmartín Rozada <manuel.luis@gmail.com>
2024-12-30 16:50:57 +0100

    Plugin olsrd: check end of file.

--- src/plugins/olsrd/olsrd.c
+++ src/plugins/olsrd/olsrd.c
@@ -286,8 +286,8 @@ static int olsrd_read_table(FILE *fh, olsrd_t *oi,
 {
     char buffer[1024];
     int lineno = 0;
-
-    while (fgets(buffer, sizeof(buffer), fh) != NULL) {
+    char *rline = NULL;
+    while ((rline = fgets(buffer, sizeof(buffer), fh)) != NULL) {
         /* An empty line ends the table. */
         size_t buffer_len = strchomp(buffer);
         if (buffer_len == 0)
@@ -300,6 +300,9 @@ static int olsrd_read_table(FILE *fh, olsrd_t *oi,
         }
         lineno++;
     }
+
+    if (rline == NULL)
+        return -1;
 
     return 0;
 }
@@ -331,6 +334,7 @@ static int olsrd_read(user_data_t *user_data)
     fputs("\r\n", fh);
     fflush(fh);
 
+    int status = 0;
     char buffer[1024];
     while (fgets(buffer, sizeof(buffer), fh) != NULL) {
         size_t buffer_len = strchomp(buffer);
@@ -338,23 +342,26 @@ static int olsrd_read(user_data_t *user_data)
             continue;
 
         if (strcmp("Table: Links", buffer) == 0) {
-            olsrd_read_table(fh, oi, olsrd_cb_links);
+            status = olsrd_read_table(fh, oi, olsrd_cb_links);
         } else if (strcmp("Table: Neighbors", buffer) == 0) {
-            olsrd_read_table(fh, oi, olsrd_cb_ignore);
+            status = olsrd_read_table(fh, oi, olsrd_cb_ignore);
         } else if (strcmp("Table: Topology", buffer) == 0) {
-            olsrd_read_table(fh, oi, olsrd_cb_topology);
+            status = olsrd_read_table(fh, oi, olsrd_cb_topology);
         } else if (strcmp("Table: HNA", buffer) == 0) {
-            olsrd_read_table(fh, oi, olsrd_cb_ignore);
+            status = olsrd_read_table(fh, oi, olsrd_cb_ignore);
         } else if (strcmp("Table: MID", buffer) == 0) {
-            olsrd_read_table(fh, oi, olsrd_cb_ignore);
+            status = olsrd_read_table(fh, oi, olsrd_cb_ignore);
         } else if (strcmp("Table: Routes", buffer) == 0) {
-            olsrd_read_table(fh, oi, olsrd_cb_routes);
+            status = olsrd_read_table(fh, oi, olsrd_cb_routes);
         } else {
            if ((strcmp("HTTP/1.0 200 OK", buffer) != 0) &&
                (strcmp("Content-type: text/plain", buffer) != 0)) {
                PLUGIN_DEBUG("Unable to handle line: %s", buffer);
             }
         }
+
+        if (status != 0)
+            break;
     }
     fclose(fh);
 
