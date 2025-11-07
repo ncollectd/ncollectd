592b49c4614dcefc86b5d8636eb812bcdab60fe8
Manuel Luis Sanmartín Rozada <manuel.luis@gmail.com>
2025-11-07 12:17:54 +0100

    New plugin sssd.

---
+++ src/plugins/sssd/sssd.c
@@ -0,0 +1,299 @@
+// SPDX-License-Identifier: GPL-2.0-only
+// SPDX-FileCopyrightText: Copyright (C) 2025 Manuel Sanmartín
+// SPDX-FileContributor: Manuel Sanmartín <manuel.luis at gmail.com>
+
+#include "plugin.h"
+#include "libutils/common.h"
+
+#include <systemd/sd-daemon.h>
+#include <systemd/sd-bus.h>
+
+#ifdef HAVE_LINUX_CONFIG_H
+#include <linux/config.h>
+#endif
+
+enum {
+    FAM_SSSD_UP,
+    FAM_SSSD_DOMAIN_ONLINE,
+    FAM_SSSD_DOMAIN_ACTIVE_SERVER,
+    FAM_SSSD_MAX
+};
+
+static metric_family_t fams[FAM_SSSD_MAX] = {
+    [FAM_SSSD_UP] = {
+        .name = "sssd_up",
+        .type = METRIC_TYPE_GAUGE,
+        .help = "Could the sssd server be reached.",
+    },
+    [FAM_SSSD_DOMAIN_ONLINE] = {
+        .name = "sssd_domain_online",
+        .type = METRIC_TYPE_GAUGE,
+        .help = "Is this domain available?"
+    },
+    [FAM_SSSD_DOMAIN_ACTIVE_SERVER] = {
+        .name = "sssd_domain_active_server",
+        .type = METRIC_TYPE_INFO,
+        .help = "Active server for this domain and service."
+    },
+};
+
+static int sssd_active_server(sd_bus *bus, char *domain, char *decode_domain, char *service)
+{
+    sd_bus_message *reply = NULL;
+    sd_bus_error error = SD_BUS_ERROR_NULL;
+
+    sd_bus_message *m = NULL;
+    int status = sd_bus_message_new_method_call(bus, &m,
+                                                "org.freedesktop.sssd.infopipe", domain,
+                                                "org.freedesktop.sssd.infopipe.Domains.Domain",
+                                                "ActiveServer");
+    if (status < 0)
+        return -1;
+
+    status = sd_bus_message_append_basic(m, 's', service);
+    if (status < 0) {
+        sd_bus_message_unref(m);
+        return -1;
+    }
+
+    status = sd_bus_call(bus, m, 0, &error, &reply);
+    if (status < 0) {
+        sd_bus_error_free(&error);
+        sd_bus_message_unref(m);
+        return -1;
+    }
+
+    sd_bus_message_unref(m);
+
+    sd_bus_error_free(&error);
+
+    char *active = NULL;
+    status = sd_bus_message_read(reply, "s", &active);
+    if (status <= 0) {
+        sd_bus_message_unref(reply);
+        return -1;
+    }
+
+    label_set_t info = {
+        .num = 1,
+        .ptr = (label_pair_t[]) {
+            {.name="active", .value=active}
+        }
+    };
+    metric_family_append(&fams[FAM_SSSD_DOMAIN_ACTIVE_SERVER], VALUE_INFO(info), NULL,
+                         &(label_pair_const_t){.name="domain", .value=decode_domain},
+                         &(label_pair_const_t){.name="service", .value=service}, NULL);
+
+    sd_bus_message_unref(reply);
+    return 0;
+}
+
+static int sssd_list_services(sd_bus *bus, char *domain, char *decode_domain)
+{
+    sd_bus_message *reply = NULL;
+    sd_bus_error error = SD_BUS_ERROR_NULL;
+
+    int status = sd_bus_call_method(bus, "org.freedesktop.sssd.infopipe", domain,
+                                         "org.freedesktop.sssd.infopipe.Domains.Domain",
+                                         "ListServices", &error, &reply, NULL);
+
+    sd_bus_error_free(&error);
+
+    if (status < 0)
+       return -1;
+
+
+    status = sd_bus_message_enter_container(reply, SD_BUS_TYPE_ARRAY, "(o)");
+    if (status < 0) {
+        sd_bus_message_unref(reply);
+        return -1;
+    }
+
+    while (true) {
+        char *service = NULL;
+
+        status = sd_bus_message_read(reply, "(s)", &service);
+        if (status <= 0)
+          break;
+
+        sssd_active_server(bus, domain, decode_domain, service);
+    }
+
+    sd_bus_message_exit_container(reply);
+    sd_bus_message_unref(reply);
+
+    return 0;
+}
+
+static int sssd_is_online(sd_bus *bus, char *domain)
+{
+    sd_bus_message *reply = NULL;
+    sd_bus_error error = SD_BUS_ERROR_NULL;
+
+    int status = sd_bus_call_method(bus, "org.freedesktop.sssd.infopipe", domain,
+                                         "org.freedesktop.sssd.infopipe,Domains.Domain",
+                                         "IsOnline", &error, &reply, NULL);
+    sd_bus_error_free(&error);
+
+    if (status < 0) 
+       return -1;
+
+    int online = 0;
+    status = sd_bus_message_read(reply, "b", &online);
+    if (status <= 0) {
+        sd_bus_message_unref(reply);
+        return -1;
+    }
+
+    sd_bus_message_unref(reply);
+    return 0;
+}
+
+static int sssd_list_domains(sd_bus *bus)
+{
+    sd_bus_message *reply = NULL;
+    sd_bus_error error = SD_BUS_ERROR_NULL;
+
+    int status = sd_bus_call_method(bus, "org.freedesktop.sssd.infopipe",
+                                         "/org/freedesktop/sssd/infopipe",
+                                         "org.freedesktop.sssd.infopipe",
+                                         "ListDomains", &error, &reply, NULL);
+    if (status < 0)
+       return -1;
+
+    sd_bus_error_free(&error);
+
+    status = sd_bus_message_enter_container(reply, SD_BUS_TYPE_ARRAY, "(o)");
+    if (status < 0) {
+        sd_bus_message_unref(reply);
+        return -1;
+    }
+
+    while (true) {
+        char *domain = NULL;
+
+        status = sd_bus_message_read(reply, "(o)", &domain);
+        if (status <= 0)
+            break;
+        if (domain == NULL)
+            break;
+        
+        char *decode_domain = NULL;
+        status = sd_bus_path_decode(domain, "/org/freedesktop/sssd/infopipe/Domains",
+                                    &decode_domain);
+        if (status < 0) 
+            continue;
+        if (decode_domain == NULL)
+            continue;
+
+        value_t online = VALUE_GAUGE(0);
+        status = sssd_is_online(bus, domain);
+        if (status == 0)
+            online = VALUE_GAUGE(1);
+
+        metric_family_append(&fams[FAM_SSSD_DOMAIN_ONLINE], online, NULL,
+                             &(label_pair_const_t){.name="domain", .value=decode_domain}, NULL);
+
+        sssd_list_services(bus, domain, decode_domain);
+
+        free(decode_domain);
+    }
+
+    sd_bus_message_exit_container(reply);
+    sd_bus_message_unref(reply);
+
+    return 0;
+}
+
+static int sssd_ping(sd_bus *bus)
+{
+    sd_bus_message *m = NULL;
+    sd_bus_message *reply = NULL;
+    sd_bus_error error = SD_BUS_ERROR_NULL;
+
+    int status = sd_bus_message_new_method_call(bus, &m, "org.freedesktop.sssd.infopipe", 
+                                                         "/org/freedesktop/sssd/infopipe",
+                                                         "org.freedesktop.sssd.infopipe",
+                                                         "Ping");
+    if (status < 0)
+        return -1;
+
+    status = sd_bus_message_append_basic(m, 's', "PING");
+    if (status < 0) {
+        sd_bus_message_unref(m);
+        return -1;
+    }
+
+    status = sd_bus_call(bus, m, 0, &error, &reply);
+    if (status < 0) {
+        sd_bus_error_free(&error);
+        sd_bus_message_unref(m);
+        return -1;
+    }
+
+    sd_bus_message_unref(m);
+
+    sd_bus_error_free(&error);
+
+    char *pong = NULL;
+    status = sd_bus_message_read(reply, "s", &pong);
+    if (status <= 0) {
+        sd_bus_message_unref(reply);
+        return -1;
+    }
+
+    if ((pong != NULL) && (strcmp(pong, "PONG") == 0)) {
+        sd_bus_message_unref(reply);
+        return 0;   
+    }
+
+    sd_bus_message_unref(reply);
+    return -1;
+}
+
+static int sssd_read(void)
+{
+    sd_bus *bus = NULL;
+
+    if (sd_booted() <= 0)
+        return -1;
+
+    if (geteuid() != 0) {
+        sd_bus_default_system(&bus);
+    } else {
+        int status = sd_bus_new(&bus);
+        if (status < 0)
+          return -1;
+
+        status = sd_bus_set_address(bus, "unix:path=/run/systemd/private");
+        if (status < 0)
+           return -1;
+
+        status = sd_bus_start(bus);
+        if (status < 0)
+           sd_bus_default_system(&bus);
+    }
+
+    int status = sssd_ping(bus);
+    if (status != 0) {
+        sd_bus_unref(bus);
+        metric_family_append(&fams[FAM_SSSD_UP], VALUE_GAUGE(0), NULL, NULL);
+        plugin_dispatch_metric_family(&fams[FAM_SSSD_UP], 0);
+        return 0;
+    }
+
+    metric_family_append(&fams[FAM_SSSD_UP], VALUE_GAUGE(1), NULL, NULL);
+
+    sssd_list_domains(bus);
+
+    sd_bus_unref(bus);
+
+    plugin_dispatch_metric_family_array(fams, FAM_SSSD_MAX, 0);
+
+    return 0;
+}
+
+void module_register(void)
+{
+    plugin_register_read("sssd", sssd_read);
+}
