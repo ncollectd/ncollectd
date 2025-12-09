4e99a4ec4654c7ab2f1c12677f100a42da0eeaf6
manuelluis <manuel.luis@gmail.com>
2025-12-09 13:07:20 +0100

    Plugin multipath: cleanup.

--- src/plugins/multipathd/multipathd.c
+++ src/plugins/multipathd/multipathd.c
@@ -1,22 +1,3 @@
-/*
- * Copyright (C) 2015 Red Hat, Inc.
- *
- * This file is part of the device-mapper multipath userspace tools.
- *
- * This program is free software; you can redistribute it and/or
- * modify it under the terms of the GNU Lesser General Public License
- * as published by the Free Software Foundation; either version 2.1
- * of the License, or (at your option) any later version.
- *
- * This program is distributed in the hope that it will be useful,
- * but WITHOUT ANY WARRANTY; without even the implied warranty of
- * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
- * GNU Lesser General Public License for more details.
- *
- * You should have received a copy of the GNU Lesser General Public License
- * along with this program.  If not, see <http://www.gnu.org/licenses/>.
- */
-
 // SPDX-License-Identifier: GPL-2.0-only
 // SPDX-FileCopyrightText: Copyright (C) 2025 Manuel Sanmartín
 // SPDX-FileContributor: Manuel Sanmartín <manuel.luis at gmail.com>
