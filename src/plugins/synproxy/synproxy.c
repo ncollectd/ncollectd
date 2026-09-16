b1a3e9ecfe126c825b66314ab9a324bb43160ca2
manuelluis <manuel.luis@gmail.com>
2026-09-16 17:20:49 +0200

    Plugin synproxy: fix incorrect metric calculation.

--- src/plugins/synproxy/synproxy.c
+++ src/plugins/synproxy/synproxy.c
@@ -28,27 +28,27 @@ static metric_family_t fams[FAM_SYNPROXY_MAX] = {
     [FAM_SYNPROXY_CONNECTIONS_SYN_RECEIVED] = {
         .name = "system_synproxy_connections_syn_received",
         .type = METRIC_TYPE_COUNTER,
-        .help = "Number of SYN received.",
+        .help = "Total number of SYN received.",
     },
     [FAM_SYNPROXY_COOKIES_INVALID] = {
         .name = "system_synproxy_cookies_invalid",
         .type = METRIC_TYPE_COUNTER,
-        .help = "Number of invalid cookies.",
+        .help = "Total number of invalid cookies.",
     },
     [FAM_SYNPROXY_COOKIES_VALID] = {
         .name = "system_synproxy_cookies_valid",
         .type = METRIC_TYPE_COUNTER,
-        .help = "Number of valid cookies.",
+        .help = "Total number of valid cookies.",
     },
     [FAM_SYNPROXY_COOKIES_RETRANSMISSION] = {
         .name = "system_synproxy_cookies_retransmission",
         .type = METRIC_TYPE_COUNTER,
-        .help = "Nymber of cookies retransmitted.",
+        .help = "Total number of cookies retransmitted.",
     },
     [FAM_SYNPROXY_CONNECTIONS_REOPENED] = {
         .name = "system_synproxy_connections_reopened",
         .type = METRIC_TYPE_COUNTER,
-        .help = "Number of connections reopened.",
+        .help = "Total number of connections reopened.",
     },
 };
 
@@ -60,58 +60,55 @@ static int synproxy_read(void)
         return -1;
     }
 
-    value_t results[SYNPROXY_FIELDS];
-    memset(results, 0, sizeof(results));
+    uint64_t results[SYNPROXY_FIELDS] = {0};
 
-    int is_header = 1, status = 0;
+    bool is_header = true;
     char buf[1024];
     while (fgets(buf, sizeof(buf), fh) != NULL) {
-        char *fields[SYNPROXY_FIELDS], *endprt;
-
         if (is_header) {
-            is_header = 0;
+            is_header = false;
             continue;
         }
 
+        char *fields[SYNPROXY_FIELDS];
         int numfields = strsplit(buf, fields, STATIC_ARRAY_SIZE(fields));
         if (numfields != SYNPROXY_FIELDS) {
-            PLUGIN_ERROR("Unexpected number of columns in %s", path_proc_synproxy);
-            status = -1;
-            break;
+            PLUGIN_ERROR("Unexpected number of columns in '%s'", path_proc_synproxy);
+            fclose(fh);
+            return -1;
         }
 
         /* 1st column (entries) is hardcoded to 0 in kernel code */
         for (size_t n = 1; n < SYNPROXY_FIELDS; n++) {
             char *endptr = NULL;
             errno = 0;
-
-            results[n] = VALUE_COUNTER(strtoull(fields[n], &endprt, 16));
-            if ((endptr == fields[n]) || errno != 0) {
-                PLUGIN_ERROR("Unable to parse value: %s", fields[n]);
+            uint64_t value = (uint64_t)strtoull(fields[n], &endptr, 16);
+            if ((endptr == fields[n]) || (errno != 0)) {
+                PLUGIN_ERROR("Unable to parse value: '%s'", fields[n]);
                 fclose(fh);
                 return -1;
             }
+
+            results[n] += value;
         }
-    }
-
-    if (status == 0) {
-        metric_family_metric_append(&fams[FAM_SYNPROXY_CONNECTIONS_SYN_RECEIVED],
-                                    (metric_t){ .value = results[1]});
-        metric_family_metric_append(&fams[FAM_SYNPROXY_COOKIES_INVALID],
-                                    (metric_t){ .value = results[2]});
-        metric_family_metric_append(&fams[FAM_SYNPROXY_COOKIES_VALID],
-                                    (metric_t){ .value = results[3]});
-        metric_family_metric_append(&fams[FAM_SYNPROXY_COOKIES_RETRANSMISSION],
-                                    (metric_t){ .value = results[4]});
-        metric_family_metric_append(&fams[FAM_SYNPROXY_CONNECTIONS_REOPENED],
-                                    (metric_t){ .value = results[5]});
-
-        plugin_dispatch_metric_family_array(fams, FAM_SYNPROXY_MAX, 0);
     }
 
     fclose(fh);
 
-    return status;
+    metric_family_append(&fams[FAM_SYNPROXY_CONNECTIONS_SYN_RECEIVED],
+                         VALUE_COUNTER(results[1]), NULL, NULL);
+    metric_family_append(&fams[FAM_SYNPROXY_COOKIES_INVALID],
+                         VALUE_COUNTER(results[2]), NULL, NULL);
+    metric_family_append(&fams[FAM_SYNPROXY_COOKIES_VALID],
+                         VALUE_COUNTER(results[3]), NULL, NULL);
+    metric_family_append(&fams[FAM_SYNPROXY_COOKIES_RETRANSMISSION],
+                         VALUE_COUNTER(results[4]), NULL, NULL);
+    metric_family_append(&fams[FAM_SYNPROXY_CONNECTIONS_REOPENED],
+                         VALUE_COUNTER(results[5]), NULL, NULL);
+
+    plugin_dispatch_metric_family_array(fams, FAM_SYNPROXY_MAX, 0);
+
+    return 0;
 }
 
 static int synproxy_init(void)
