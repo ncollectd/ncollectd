ee816044186613a48528491ad3989b759c28308d
manuelluis <manuel.luis@gmail.com>
2026-09-17 12:29:42 +0200

    Plugin cpusleep: better time diff calculation.

--- src/plugins/cpusleep/cpusleep.c
+++ src/plugins/cpusleep/cpusleep.c
@@ -2,15 +2,8 @@
 // SPDX-FileCopyrightText: Copyright (C) 2016 rinigus
 // SPDX-FileContributor: rinigus <http://github.com/rinigus>
 
-/*
- * CPU sleep is reported in milliseconds of sleep per second of wall
- * time. For that, the time difference between BOOT and MONOTONIC clocks
- * is reported using derive type.
- */
-
 #include "plugin.h"
-
-#include <time.h>
+#include "libutils/time.h"
 
 static metric_family_t fam = {
     .name = "system_cpusleep_seconds",
@@ -20,23 +13,19 @@ static metric_family_t fam = {
 
 static int cpusleep_read(void)
 {
-    struct timespec b;
-    if (clock_gettime(CLOCK_BOOTTIME, &b) < 0) {
+    struct timespec ts_boot;
+    if (clock_gettime(CLOCK_BOOTTIME, &ts_boot) < 0) {
         PLUGIN_ERROR("clock_boottime failed");
         return -1;
     }
 
-    struct timespec m;
-    if (clock_gettime(CLOCK_MONOTONIC, &m) < 0) {
+    struct timespec ts_monotonic;
+    if (clock_gettime(CLOCK_MONOTONIC, &ts_monotonic) < 0) {
         PLUGIN_ERROR("clock_monotonic failed");
         return -1;
     }
 
-    // to avoid false positives in counter overflow due to reboot,
-    // derive is used. Sleep is calculated in milliseconds
-    double diffsec = b.tv_sec - m.tv_sec;
-    double diffnsec = b.tv_nsec - m.tv_nsec;
-    double sleep = diffsec + diffnsec / 100000000.0;
+    double sleep = TIMESPEC_TO_DOUBLE(&ts_boot) - TIMESPEC_TO_DOUBLE(&ts_monotonic);
 
     metric_family_append(&fam, VALUE_COUNTER_FLOAT64(sleep), NULL, NULL);
 
