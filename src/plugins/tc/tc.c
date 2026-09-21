eafcf45323a863a8d9e542073d61febc7f8a3e62
Manuel Luis Sanmartín Rozada <manuel.luis@gmail.com>
2026-09-21 17:52:59 +0200

    Plugin tc: check ifi_index >= 0.

--- src/plugins/tc/tc.c
+++ src/plugins/tc/tc.c
@@ -261,23 +261,24 @@ static int update_iflist(struct ifinfomsg *msg, const char *dev)
 {
     /* Update the `iflist'. It's used to know which interfaces exist and query
      * them later for qdiscs and classes. */
-    if ((msg->ifi_index >= 0) && ((size_t)msg->ifi_index >= iflist_len)) {
-        char **temp;
+    if (msg->ifi_index >= 0) {
+        if ((size_t)msg->ifi_index >= iflist_len) {
+            char **temp = realloc(iflist, (msg->ifi_index + 1) * sizeof(char *));
+            if (temp == NULL) {
+                PLUGIN_ERROR("update_iflist: realloc failed.");
+                return -1;
+            }
 
-        temp = realloc(iflist, (msg->ifi_index + 1) * sizeof(char *));
-        if (temp == NULL) {
-            PLUGIN_ERROR("update_iflist: realloc failed.");
-            return -1;
+            memset(temp + iflist_len, '\0', (msg->ifi_index + 1 - iflist_len) * sizeof(char *));
+            iflist = temp;
+            iflist_len = msg->ifi_index + 1;
         }
 
-        memset(temp + iflist_len, '\0', (msg->ifi_index + 1 - iflist_len) * sizeof(char *));
-        iflist = temp;
-        iflist_len = msg->ifi_index + 1;
-    }
-    if ((iflist[msg->ifi_index] == NULL) || (strcmp(iflist[msg->ifi_index], dev) != 0)) {
-        if (iflist[msg->ifi_index] != NULL)
-            free(iflist[msg->ifi_index]);
-        iflist[msg->ifi_index] = strdup(dev);
+        if ((iflist[msg->ifi_index] == NULL) || (strcmp(iflist[msg->ifi_index], dev) != 0)) {
+            if (iflist[msg->ifi_index] != NULL)
+                free(iflist[msg->ifi_index]);
+            iflist[msg->ifi_index] = strdup(dev);
+        }
     }
 
     return 0;
